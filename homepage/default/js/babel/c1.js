1f42701412cccf5f7baedc211ffee81659ac655d
David <davidthanphilom@msn.com>
2020-01-21 14:54:19 -0800

    pulled from promo.holiday.12.10 promos/one-offs homepage/ mega-menu master

--- homepage/default/js/babel/c1.js
+++ homepage/default/js/babel/c1.js
@@ -1,36 +1,30 @@
 "use strict";
 
-var c1 = [// {
-//   elem: '.slide0 .cta1',
-//   url: '/girls-clothing/favorites/limited-time-sale',
-//   text: '<span>shop</span> girl'
-// },
-// {
-//   elem: '.slide0 .cta2',
-//   url: '/boys-clothing/favorites/limited-time-sale',
-//   text: '<span>shop</span> boy'
-// },
-// {
-//   elem: '.slide0 .cta3',
-//   url: '/tween-girls-clothing/favorites/limited-time-sale',
-//   text: '<span>shop</span> tween'
-// },
-// {
-//   elem: '.slide0 .cta4',
-//   url: '/baby-girl-clothes/favorites/limited-time-sale',
-//   text: '<span>shop</span> baby girl'
-// },
-// {
-//   elem: '.slide0 .cta5',
-//   url: '/baby-boy-clothes/favorites/limited-time-sale',
-//   text: '<span>shop</span> baby boy'
-// },
-// {
-//   elem: '.slide0 .cta6',
-//   url: '/select-styles',
-//   text: '<span>shop</span> the sale'
-// },
-{
+var c1 = [{
+  elem: '.slide0 .cta1',
+  url: '/girls-clothing',
+  text: '<span>shop</span> girl'
+}, {
+  elem: '.slide0 .cta2',
+  url: '/boys-clothing',
+  text: '<span>shop</span> boy'
+}, {
+  elem: '.slide0 .cta3',
+  url: '/tween-girls-clothing',
+  text: '<span>shop</span> tween'
+}, {
+  elem: '.slide0 .cta4',
+  url: '/baby-girl-clothes',
+  text: '<span>shop</span> baby girl'
+}, {
+  elem: '.slide0 .cta5',
+  url: '/baby-boy-clothes',
+  text: '<span>shop</span> baby boy'
+}, {
+  elem: '.slide0 .cta6',
+  url: '/clothing-sale',
+  text: '<span>shop</span> sale'
+}, {
   elem: '.slide1 .cta1',
   url: '/baby-girl-clothes/new-arrivals',
   text: '<span>shop</span> baby girl'
