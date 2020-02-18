6a3d73a5e801884a2e3881830adb81011d6f4d7b
David <davidthanphilom@msn.com>
2020-02-18 10:03:22 -0800

    promos/one-offs mega-menu/ pulled from promos.one-offs.2.13

--- promos/one-offs/default/js/babel/scripts.js
+++ promos/one-offs/default/js/babel/scripts.js
@@ -1,31 +1,19 @@
 "use strict";
 
 jQuery(document).ready(function () {
-  //array of urls to hide the banners
-  var urlArray = [{
-    name: 'hp',
-    url: '/'
-  } // {
-  // 	name: 'black-friday',
-  // 	url: '/black-friday'
-  // },
-  // {
-  // 	name:'sale-page',
-  // 	url:'/clothing-sale'
-  // },
-  ]; //function to hide or show the banner
+  var first = $('.the-promo').find('.span-wrap').clone();
+  var shopNow = $('.the-promo').find('.shop-now').clone(); // $('.hp-promo div').append(first, shopNow);
 
-  var hideBanners = function hideBanners() {
-    var url = window.location.pathname;
-    $.each(urlArray, function (i, val) {
-      if (url == val.url) {
-        $('div[data-zone="sitewide_banner"]').empty();
-      }
-    });
-  }; //on page load call hideBanners
+  $('.hp-promo div').append(shopNow);
+  var url = window.location.pathname;
 
+  if (url == '/') {
+    $('.hp-promo').show();
+    $('.hp').before($('.hp-promo'));
+  } else {
+    $('.the-promo').show();
+  }
 
-  hideBanners();
   $('.the-promo .see-details').fancybox();
   $('.close-fancy').click(function (e) {
     // e.preventDefault();
--- promos/one-offs/default/js/scripts.js
+++ promos/one-offs/default/js/scripts.js
@@ -1,42 +1,25 @@
 jQuery(document).ready(function() {
 
-  //array of urls to hide the banners
-  var urlArray = [{
-      name: 'hp',
-      url: '/',
-    },
-    // {
-    // 	name: 'black-friday',
-    // 	url: '/black-friday'
-    // },
-    // {
-    // 	name:'sale-page',
-    // 	url:'/clothing-sale'
-    // },
-  ];
+    const first = $('.the-promo').find('.span-wrap').clone();
+    const shopNow = $('.the-promo').find('.shop-now').clone();
+    // $('.hp-promo div').append(first, shopNow);
+    $('.hp-promo div').append(shopNow);
 
-  //function to hide or show the banner
-  var hideBanners = function() {
-    var url = window.location.pathname;
+    const url = window.location.pathname;
 
-    $.each(urlArray, function(i, val) {
-      if (url == val.url) {
-        $('div[data-zone="sitewide_banner"]').empty();
-      }
+    if(url == '/'){
+        $('.hp-promo').show();
+        $('.hp').before($('.hp-promo'));
+    } else {
+        $('.the-promo').show();
+    }
+    
+
+    $('.the-promo .see-details').fancybox();
+
+    $('.close-fancy').click(function(e) {
+        // e.preventDefault();
+        $.fancybox.close();
     });
-  };
-
-  //on page load call hideBanners
-  hideBanners();
-
-  $('.the-promo .see-details').fancybox();
-
-
-  $('.close-fancy').click(function(e) {
-    // e.preventDefault();
-    $.fancybox.close();
-
-  });
-
 
 });
