60ed60adae7e51efb35cd1cdc25e9f6b8aa79900
David <davidthanphilom@msn.com>
2020-03-03 13:15:34 -0800

    homepage/ mega-menu/ landing-page/shops/special-occasion-shop pulled from refresh.2.20  master

--- homepage/default/js/babel/scripts.js
+++ homepage/default/js/babel/scripts.js
@@ -2,24 +2,26 @@
 
 var addAlt = require('./alt.js');
 
-addAlt('.hp .c1 img', '2 girls');
-addAlt('.hp .c3 .first img', 'girl');
-addAlt('.hp .c3 .second img', '2 girls');
+addAlt('.hp .c1 img', 'girl and boy');
+addAlt('.hp .c3 .first img', 'boy');
+addAlt('.hp .c3 .second img', 'baby girl');
 addAlt('.hp .c3 .third img', 'baby');
 addAlt('.hp .c4 .first img', '2 girls');
 addAlt('.hp .c4 .second img', 'boy');
 addAlt('.hp .c5 img', 'girls and boy in swimwear');
-addAlt('.hp .c6 .first img', 'boy and girl');
-addAlt('.hp .c6 .second img', 'tea gives back 10% of profits to ensure a better world for kids everywhere');
-addAlt('.hp .c6 .third img', '2 girls');
+addAlt('.hp .c6 .first img', 'newborn');
+addAlt('.hp .c6 .second img', 'boy and girl in pjs');
+addAlt('.hp .c7 .first img', 'girl');
+addAlt('.hp .c7 .second img', 'tea gives back 10% of profits to ensure a better world for kids everywhere');
+addAlt('.hp .c7 .third img', 'boy and girl');
 
 var inview = require('./inview.js');
 
-inview('.hp .c1, .hp .c2, .hp .c3, .hp .c4, .hp .c5, .hp .c6');
+inview('.hp .c1, .hp .c2, .hp .c3, .hp .c4, .hp .c5, .hp .c6, .hp .c7');
 
 var addTracking = require('./tracking.js');
 
-for (var i = 1; i < 6; i++) {
+for (var i = 1; i < 8; i++) {
   addTracking('.hp .c' + i + ' a', 'c' + i);
 }
 //# sourceMappingURL=scripts.js.map
---
+++ landing-page/shops/special-occasion-shop/default/js/babel/scripts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+  /*const baseUrl = `https://www.teacollection.com`;
+  var imgArr = $('.content-wrap').find('img');
+  $.each(imgArr, function(i, val) {
+      var src = $(val).attr('src');
+      $(val).attr('src', baseUrl + src);
+  });*/
+});
+//# sourceMappingURL=scripts.js.map
