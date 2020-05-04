e8b6fd9e2fac708d722e426ede889645e946d1fc
David <davidthanphilom@msn.com>
2020-05-04 14:31:16 -0700

    pulled from promo.friends.family.4.27 homepage/ promos/friends-family mega-menu/ master

--- mega-menu/js/sale.js
+++ mega-menu/js/sale.js
@@ -7,54 +7,65 @@ $(document).ready(function() {
         var promo1 = href;
         var promo2 = href;
 
-        /*
+
         switch (href) {
             //sale to new arrivals
             case '/sale/girls-clothing/view-all':
+                promo2 = '/girls-pajamas-sleepwear';
+                break;
+
             case '/sale/boys-clothing/view-all':
+                promo2 = '/boys-pajamas-sleepwear';
+                break;
+
             case '/sale/baby-girl-clothes/view-all':
+                promo2 = '/baby-girl-pajamas';
+                break;
+
             case '/sale/baby-boy-clothes/view-all':
-
-
-                promo1 = promo1.replace('/sale', '');
-                promo1 = promo1.replace('view-all', 'new-arrivals');
+                promo2 = '/baby-boy-pajamas';
                 break;
 
             case '/sale/toddler-girls-clothing/view-all':
-            case '/sale/toddler-boys-clothing/view-all':
-            case '/sale/newborn-clothing/view-all':
-
-                promo1 = promo1.replace('/sale', '');
-                promo1 = promo1.replace('ing/view-all', 'es/favorites/new-arrivals');
+                promo2 = '/toddler-girls-clothes/pajamas';
                 break;
 
+            case '/sale/toddler-boys-clothing/view-all':
+                promo2 = '/toddler-boys-clothes/pajamas';
+                break;
 
+            case '/sale/newborn-clothing/view-all':
             case '/sale/tween-girls-clothing/view-all':
+                promo2 = '/pajamas';
+                break;
 
-                promo1 = promo1.replace('/sale', '');
-                promo1 = promo1.replace('view-all', 'favorites/new-arrivals');
-                break
-
-                //new arrivals to sale
-                // case '/girls-clothing/new-arrivals':
-                // case '/boys-clothing/new-arrivals':
-                // case '/baby-girl-clothes/new-arrivals':
-                // case '/baby-boy-clothes/new-arrivals':
-
-                //     promo1 = promo1.replace('new-arrivals', '');
-                //     promo1 = `/sale${promo1}`;
-
+                // case '/sale/tween-girls-clothing/view-all':
+                //     promo2 = '';
                 //     break;
 
-                // case '/toddler-girls-clothes/favorites/new-arrivals':
-                // case '/toddler-boys-clothes/favorites/new-arrivals':
-                // case '/newborn-clothes/favorites/new-arrivals':
+                //new arrivals to view all
+            case '/girls-clothing/new-arrivals':
+            case '/boys-clothing/new-arrivals':
+            case '/baby-girl-clothes/new-arrivals':
+            case '/baby-boy-clothes/new-arrivals':
 
-                //     promo1 = promo1.replace('es/favorites/new-arrivals', 'ing');
-                //     promo1 = `/sale${promo1}`;
+                promo1 = promo1.replace('new-arrivals', 'view-all');
+                // promo1 = `/sale${promo1}`;
 
-                //     break;
+                break;
 
+            case '/toddler-girls-clothes/favorites/new-arrivals':
+            case '/toddler-boys-clothes/favorites/new-arrivals':
+            case '/newborn-clothes/favorites/new-arrivals':
+            case '/tween-girls-clothing/favorites/new-arrivals':
+
+                promo1 = promo1.replace('favorites/new-arrivals', 'view-all');
+                // promo1 = `/sale${promo1}`;
+
+                break;
+
+            case '/newborn-clothes/':
+                promo1 = `${promo1}view-all`;
 
                 // case '/tween-girls-clothing/favorites/new-arrivals':
 
@@ -66,12 +77,12 @@ $(document).ready(function() {
             default:
                 // code block
         }
-        */
+
 
         // console.log(promo1);
         $('.promo1-url').attr('href', promo1);
-        // $('.promo2-url').attr('href', promo2);
-        $('.promo1-url').show();
+        $('.promo2-url').attr('href', promo2);
+        $('.promo1-url, .promo2-url').show();
 
 
     }, function() {
