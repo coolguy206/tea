054ced9bcc8bfb449be1033e1d3e71d8a86d2a4a
David Thanphilom <davidthanphilom@msn.com>
2020-07-03 11:30:43 -0700

    pulled from promo.one-offs.5.28 mega-menu/ homepage/ promos/one-offs master

--- mega-menu/js/sale.js
+++ mega-menu/js/sale.js
@@ -1,54 +1,84 @@
 $(document).ready(function() {
+    var urls = {
+        girl: `/girls-dresses`,
+        toddler: `/toddler-girls-clothes/dresses`,
+        tween: `/tween-girls-clothing/category/dresses`,
+        babyGirl: `/baby-girl-dresses`,
+        sale: {
+            girl: `/sale/girls-dresses`,
+            toddler: `/sale/toddler-girls-dresses`,
+            tween: `/sale/tween-girls-dresses`,
+            babyGirl: `/sale/baby-dresses`
+        }
+    };
+    $('.dept-girl, .dept-baby').hover(function() {
+        $('.promo1-url').show();
+    }, function() {
+
+    });
+    $('.dept-boy').hover(function() {
+        $('.promo1-url').hide();
+    }, function() {
+
+    });
+
     $('.site-nav-submenu .bar>div').hover(function() {
         var href = $(this).find('a').first().attr('href');
-        console.log(href);
+        // console.log(href);
 
 
         var promo1 = href;
         var promo2 = href;
 
+        $('.promo1-url').hide();
+
 
         switch (href) {
             // sale to new arrivals
-            case '/sale/girls-clothing/view-all':
-                promo1 = '/girls-clothing/new-arrivals';
-                promo2 = '/sale/girls-clothing/clearance';
-                break;
+            // case '/sale/girls-clothing/view-all':
+            // promo1 = urls.girl;
+            //     promo2 = '/sale/girls-clothing/clearance';
+            //     break;
 
             case '/sale/boys-clothing/view-all':
-                promo1 = '/boys-clothing/new-arrivals';
+            case '/boys-clothing/new-arrivals':
+                // promo1 = '/boys-clothing/new-arrivals';
                 promo2 = '/sale/boys-clothing/clearance';
                 break;
 
-            case '/sale/baby-girl-clothes/view-all':
-                promo1 = '/baby-girl-clothes/new-arrivals';
-                promo2 = '/sale/baby-girl-clothes/clearance';
-                break;
+                // case '/sale/baby-girl-clothes/view-all':
+                //     promo1 = '/baby-girl-clothes/new-arrivals';
+                //     promo2 = '/sale/baby-girl-clothes/clearance';
+                //     break;
 
             case '/sale/baby-boy-clothes/view-all':
-                promo1 = '/baby-boy-clothes/new-arrivals';
+            case '/baby-boy-clothes/new-arrivals':
+                // promo1 = '/baby-boy-clothes/new-arrivals';
                 promo2 = '/sale/baby-boy-clothes/clearance';
                 break;
 
-            case '/sale/toddler-girls-clothing/view-all':
-                promo1 = '/toddler-girls-clothes/favorites/new-arrivals';
-                promo2 = '/sale/toddler-girls-clothing/clearance';
-                break;
+                // case '/sale/toddler-girls-clothing/view-all':
+                //     promo1 = '/toddler-girls-clothes/favorites/new-arrivals';
+                //     promo2 = '/sale/toddler-girls-clothing/clearance';
+                //     break;
 
             case '/sale/toddler-boys-clothing/view-all':
-                promo1 = '/toddler-boys-clothes/favorites/new-arrivals';
+            case '/toddler-boys-clothes/favorites/new-arrivals':
+                // promo1 = '/toddler-boys-clothes/favorites/new-arrivals';
                 promo2 = '/sale/toddler-boys-clothing/clearance';
                 break;
 
             case '/sale/newborn-clothing/view-all':
-                promo1 = '/newborn-clothes/favorites/new-arrivals';
+            case '/newborn-clothes/favorites/new-arrivals':
+            case '/newborn-clothes/':
+                // promo1 = '/newborn-clothes/favorites/new-arrivals';
                 promo2 = '/sale/newborn-clothing/clearance';
                 break;
 
-            case '/sale/tween-girls-clothing/view-all':
-                promo1 = '/tween-girls-clothing/favorites/new-arrivals';
-                promo2 = '/sale/tween-girls-clothing/clearance';
-                break;
+                // case '/sale/tween-girls-clothing/view-all':
+                //     promo1 = '/tween-girls-clothing/favorites/new-arrivals';
+                //     promo2 = '/sale/tween-girls-clothing/clearance';
+                //     break;
 
                 // new arrivals to view all
                 // case '/girls-clothing/new-arrivals':
@@ -98,10 +128,52 @@ $(document).ready(function() {
                 //     $('.promo1-url, .promo2-url').show();
                 //     break;
 
-            case '/newborn-clothes/':
-                promo1 = `/newborn-clothes/favorites/new-arrivals`;
-                break;
+                // case '/newborn-clothes/':
+                //     promo1 = `/newborn-clothes/favorites/new-arrivals`;
+                //     break;
 
+                //show girls categories
+            case '/girls-clothes':
+            case '/girls-clothing/new-arrivals':
+                $('.promo1-url').show();
+                promo1 = urls.girl;
+                promo2 = '/sale/girls-clothing/clearance';
+                break;
+            case '/sale/girls-clothing/view-all':
+                $('.promo1-url').show();
+                promo1 = urls.sale.girl;
+                promo2 = '/sale/girls-clothing/clearance';
+                break;
+            case '/baby-girl-clothes/new-arrivals':
+                $('.promo1-url').show();
+                promo1 = urls.babyGirl;
+                promo2 = '/sale/baby-girl-clothes/clearance';
+                break;
+            case '/sale/baby-girl-clothes/view-all':
+                $('.promo1-url').show();
+                promo1 = urls.sale.babyGirl;
+                promo2 = '/sale/baby-girl-clothes/clearance';
+                break;
+            case '/toddler-girls-clothes/favorites/new-arrivals':
+                $('.promo1-url').show();
+                promo1 = urls.toddler;
+                promo2 = '/sale/toddler-girls-clothing/clearance';
+                break;
+            case '/sale/toddler-girls-clothing/view-all':
+                $('.promo1-url').show();
+                promo1 = urls.sale.toddler;
+                promo2 = '/sale/toddler-girls-clothing/clearance';
+                break;
+            case '/tween-girls-clothing/favorites/new-arrivals':
+                $('.promo1-url').show();
+                promo1 = urls.tween;
+                promo2 = '/sale/tween-girls-clothing/clearance';
+                break;
+            case '/sale/tween-girls-clothing/view-all':
+                $('.promo1-url').show();
+                promo1 = urls.sale.tween;
+                promo2 = '/sale/tween-girls-clothing/clearance';
+                break;
             default:
                 break;
 
