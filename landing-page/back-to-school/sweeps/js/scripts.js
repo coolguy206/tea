f8fbf4dfe30c978e90197ed1b3d48a0bdb1f0e35
David Thanphilom <davidthanphilom@msn.com>
2020-08-10 15:44:39 -0700

    pulled from landing.back-to-school.sweeps.8.4 landing-page/back-to-school/sweeps master

---
+++ landing-page/back-to-school/sweeps/js/scripts.js
@@ -0,0 +1,97 @@
+import {
+  keys
+} from './api.js';
+// console.log(keys);
+
+$(document).ready(function() {
+  const url = window.location.href;
+  const ac = "Sweepstakes_Aug2020";
+
+  function emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  }
+
+  $('.this-form form button').click(function(e) {
+    e.preventDefault();
+    // console.log(e.target);
+    var fname = $('.this-form input[name="fname"]').val();
+    var lname = $('.this-form input[name="lname"]').val();
+    var email = $('.this-form input[name="email"]').val();
+    var checkbox = $('.this-form input[type=checkbox]').prop('checked');
+    var valid = emailIsValid(email);
+    // console.log(`
+    //   fname: ${fname}
+    //   lname: ${lname}
+    //   email: ${email}
+    //   checkbox: ${checkbox}
+    //   valid: ${valid}`);
+
+    if (valid && checkbox && fname !== '' && lname !== '') {
+      $('.this-form form').hide();
+      $('.this-form .sending').show();
+
+      Sailthru.integration("userSignUp", {
+        "id": email,
+        "email": email,
+        "lists": {
+          // "MASTER_CONTACTS_LIST": 1
+          "Sweepstakes-Aug2020": 1
+        },
+        "vars": {
+          "ACQUISITION_SOURCE": ac,
+          "FIRST_NAME": fname,
+          "LAST_NAME": lname
+        },
+        "source": ac,
+        "onSuccess": function() {
+          $('.this-form .sending').hide();
+          $('.this-form .successfully-sent').show();
+
+          window.dataLayer.push({
+            'event': 'sailthru',
+            'theUrl': url,
+            'sailthruEmail': email,
+            'sailthruSource': ac
+          });
+
+          var pdpObj = `{"id": "${email}", "optout_email": "none"}`;
+          // console.log(pdpObj);
+
+          var sig = md5(`${keys.secret}${keys.api}json${pdpObj}`);
+          // console.log(sig);
+
+          var baseUrl = `https://api.sailthru.com/user`;
+
+          var data = new FormData();
+          data.append("api_key", keys.api);
+          data.append("format", "json");
+          data.append("json", pdpObj);
+          data.append("sig", sig);
+
+          var xhr = new XMLHttpRequest();
+          xhr.withCredentials = true;
+
+          xhr.addEventListener("readystatechange", function() {
+            if (this.readyState === 4) {
+              console.log(this.responseText);
+            }
+          });
+
+          xhr.open("POST", baseUrl);
+
+          xhr.send(data);
+        }
+      });
+
+    } else {
+      console.log('error');
+      $('.this-form .sending').hide();
+      $('.this-form form').show();
+      $('.this-form .this-error').css('display', 'block');
+      $('.this-form input[name="fname"], .this-form input[name="lname"], .this-form input[name="email"], .this-form input[type=checkbox]').addClass('error-border')
+      // $('.this-form input').focus();
+    }
+  });
+
+
+});
