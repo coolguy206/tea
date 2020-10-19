d8f1025c7f2e2c04915aa0227561288a61ff3273
David Thanphilom <davidthanphilom@msn.com>
2020-10-19 13:30:01 -0700

    pulled from 9.17.homepage homepage/ master

--- homepage/default/js/browserify/scripts.js
+++ homepage/default/js/browserify/scripts.js
@@ -87,19 +87,23 @@ module.exports = function (elem) {
 
 var addAlt = require('./alt.js');
 
-addAlt('.hp .c1 img', 'girl in pink dress');
-addAlt('.hp .c2 .first img', 'baby girl in stripe ddress');
-addAlt('.hp .c2 .second img', 'baby in green romper');
-addAlt('.hp .c2 .third img', 'baby boy in camo pants');
-addAlt('.hp .c3 .first img', 'boy in light blue shirt');
-addAlt('.hp .c3 .second img', 'girls in pattern dresses');
-addAlt('.hp .c4 .first img', 'newborn in yellow onesies');
-addAlt('.hp .c4 .second img', 'baby blue pajamas');
-addAlt('.hp .c5 .first img', 'sneak a peak at sale');
-addAlt('.hp .c5 .second img', 'treasures from seasons past');
-addAlt('.hp .c7 .first img', 'lama eating grass');
-addAlt('.hp .c7 .second img', 'tea gives back 10% of profits to ensure a better world for kids everywher');
-addAlt('.hp .c7 .third img', 'girls in pink dress');
+addAlt('.hp .c1 img', 'girl in blue overalls');
+addAlt('.hp .c2 .first img', 'baby girl in purple ddress');
+addAlt('.hp .c2 .second img', 'baby girl pink romper');
+addAlt('.hp .c2 .third img', 'baby boy in blue outfit');
+addAlt('.hp .c3 .first img', 'girl in blue dress');
+addAlt('.hp .c3 .second img', 'boy in blue outfit');
+addAlt('.hp .c4 .first img', 'tea logo');
+addAlt('.hp .c4 .second img', 'boy in PJs');
+addAlt('.hp .c5 .first img', 'girl in purple dress');
+addAlt('.hp .c5 .second img', 'clothes lay down');
+addAlt('.hp .c5 .third img', 'sisters in matching pattern dress');
+addAlt('.hp .c5 .fourth img', 'boy in blue athletic wear');
+addAlt('.hp .c6 .first img', 'baby in pink rompers');
+addAlt('.hp .c6 .second img', 'clearance');
+addAlt('.hp .c7 .first img', 'girl making desserts');
+addAlt('.hp .c7 .second img', 'family sitting together');
+addAlt('.hp .c7 .third img', 'boy in blue outfit');
 
 var inview = require('./inview.js');
 
