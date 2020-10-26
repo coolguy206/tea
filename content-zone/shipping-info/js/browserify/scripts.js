3a29732f664d7fd95ffe9e769e98f09cfda6a018
David Thanphilom <davidthanphilom@msn.com>
2020-10-26 14:37:59 -0700

    content-zone/shipping-info content-zone/shipping-message changed messaging master

---
+++ content-zone/shipping-info/js/browserify/scripts.js
@@ -0,0 +1,21 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+$(document).ready(function () {//if shipping checkout page hide .order-history
+  // var url = window.location.href;
+  // if (url.indexOf('?')) {
+  //   console.log('yes');
+  //   var page = url.split('?');
+  //if shipping page
+  //   if (page[1] == 'page=shipping' || page[1] == 'page=tea&action=guest_checkout') {
+  //     console.log('its shipping');
+  //     //hide .order-history
+  //     setTimeout(function() {
+  //       $('.order-history').hide();
+  //     }, 1500);
+  //   }
+  // }
+});
+
+
+},{}]},{},[1]);
