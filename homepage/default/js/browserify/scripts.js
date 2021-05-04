e4e139ab7b8a7add874226eff384fc327ea06b7c
David Thanphilom <davidthanphilom@msn.com>
2021-05-04 12:56:50 -0700

    pulled from 04.22.hp homepage/ master

--- homepage/default/js/browserify/scripts.js
+++ homepage/default/js/browserify/scripts.js
@@ -94,23 +94,23 @@ var inview = require('./inview.js');
 var addTracking = require('./tracking.js');
 
 $(document).ready(function () {
-  addAlt('.hp .c1 img', 'girls in purple & pink dresses');
-  addAlt('.hp .c2 .first img', 'boy in plaid shirt');
-  addAlt('.hp .c2 .second img', 'baby in yellow romper');
-  addAlt('.hp .c2 .third img', 'girl in glue green dress');
-  addAlt('.hp .c3 .first img', 'kids in plaid clothes');
-  addAlt('.hp .c3 .second img', 'boy in dark blue swimwear');
+  addAlt('.hp .c1 img', 'family on van in tea summer clothes');
+  addAlt('.hp .c2 .first img', 'girl in green bird print dress');
+  addAlt('.hp .c2 .second img', 'baby in rainbow stripe romper');
+  addAlt('.hp .c2 .third img', 'boy in aqua shark tee and shorts');
+  addAlt('.hp .c3 .first img', 'pineapple dress for baby girl and girl');
+  addAlt('.hp .c3 .second img', 'boy in fish graphic shorts swimwear');
   addAlt('.hp .c4 .first img', 'plaid outfits');
   addAlt('.hp .c4 .second img', 'boy in PJs');
   addAlt('.hp .c5 .first img', 'girl in green dress');
   addAlt('.hp .c5 .second img', 'baby sets outfits');
   addAlt('.hp .c5 .third img', 'red orange pajamas');
   addAlt('.hp .c5 .fourth img', 'boy in stripe sweater');
-  addAlt('.hp .c6 .first img', 'baby in lion outfit');
-  addAlt('.hp .c6 .second img', 'baby in blue striped dress');
-  addAlt('.hp .c7 .first img', 'azulejo');
+  addAlt('.hp .c6 .first img', 'baby in fish and jelly fish outfit');
+  addAlt('.hp .c6 .second img', 'girls in striped dress and top');
+  addAlt('.hp .c7 .first img', 'summer beach activity');
   addAlt('.hp .c7 .second img', 'learn how #teagivesback');
-  addAlt('.hp .c7 .third img', 'boys in floral shirt and girl in floral dress');
+  addAlt('.hp .c7 .third img', 'boys in car graphic tee and pineapple shorts');
   inview('.hp .c1, .hp .c2, .hp .c3, .hp .c4, .hp .c5, .hp .c6, .hp .c7');
 
   for (var i = 1; i < 8; i++) {
