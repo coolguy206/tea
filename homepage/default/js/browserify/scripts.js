2618a1c310cc341dbc4c7d0752fd4912ed4fefa4
David Thanphilom <davidthanphilom@msn.com>
2021-10-12 14:40:34 -0700

    pulled from 09.02.hp homepage/ master

--- homepage/default/js/browserify/scripts.js
+++ homepage/default/js/browserify/scripts.js
@@ -96,22 +96,22 @@ var addTracking = require('./tracking.js');
 $(document).ready(function () {
   addAlt('.hp .c1 img', 'kids on a bike');
   addAlt('.hp .c2 .first img', 'girl in flower graphic dress');
-  addAlt('.hp .c2 .second img', 'baby in yellow romper');
+  addAlt('.hp .c2 .second img', 'baby in moose graphic romper');
   addAlt('.hp .c2 .third img', 'girl in strip dress');
-  addAlt('.hp .c3 .first img', 'kid in bike graphic tee');
-  addAlt('.hp .c3 .second img', 'girl in rainbow top');
-  addAlt('.hp .c4 .first img', 'baby in yellow dress');
-  addAlt('.hp .c4 .second img', 'strawberry and rainbow PJs');
-  addAlt('.hp .c4 .third img', 'baby in orange slice graphic oneies');
+  addAlt('.hp .c3 .first img', 'kids in mountain & moose graphic tee');
+  addAlt('.hp .c3 .second img', 'girl in butterfly top');
+  addAlt('.hp .c4 .first img', 'halloween shop');
+  addAlt('.hp .c4 .second img', 'baby in yellow flower dress');
+  addAlt('.hp .c4 .third img', 'strawberry & rainbow PJs');
   addAlt('.hp .c5 .first img', 'boy in motorcycle graphic tee');
   addAlt('.hp .c5 .second img', 'fish & seahorse outfit');
   addAlt('.hp .c5 .third img', 'baby in strawberry graphic oneies');
   addAlt('.hp .c5 .fourth img', 'boy in stripe sweater');
   addAlt('.hp .c6 .first img', 'baby in fish and jelly fish outfit');
   addAlt('.hp .c6 .second img', 'girls in striped dress and top');
-  addAlt('.hp .c7 .first img', 'girl with toy horse on her head');
+  addAlt('.hp .c7 .first img', 'kids in apple graphic PJs with swedish pancakes');
   addAlt('.hp .c7 .second img', 'learn how #teagivesback');
-  addAlt('.hp .c7 .third img', 'siblings in Tea Collection clothes');
+  addAlt('.hp .c7 .third img', 'kid in green Tea Collection outfit');
   inview('.hp .c1, .hp .c2, .hp .c3, .hp .c4, .hp .c5, .hp .c6, .hp .c7');
 
   for (var i = 1; i < 8; i++) {
