ece164ac9fdfbd0177bc533492cf396dcb865b7e
David Thanphilom <davidthanphilom@msn.com>
2021-10-12 14:26:47 -0700

    pulled from 08.12.promo.one-offs homepage/ mega-menu/ promos/one-offs master

--- mega-menu/js/babel/urls.js
+++ mega-menu/js/babel/urls.js
@@ -6,17 +6,17 @@ Object.defineProperty(exports, "__esModule", {
 exports.urls = void 0;
 var urls = {
   promo1: {
-    girl: "/girls-clothing/favorites/limited-time-sale",
-    boy: "/boys-clothing/favorites/limited-time-sale",
+    girl: "/girls-clothes/",
+    boy: "/boys-clothes/",
     tween: "/tween-girls-clothing",
     baby: {
-      girl: "/baby-girl-clothes/favorites/limited-time-sale",
-      boy: "/baby-boy-clothes/favorites/limited-time-sale",
-      newborn: "/newborn-clothing/favorites/limited-time-sale"
+      girl: "/baby-girl-clothes/",
+      boy: "/baby-boy-clothes/",
+      newborn: "/newborn-clothes/favorites/new-arrivals"
     },
     toddler: {
-      girl: "/girls-clothing/favorites/limited-time-sale",
-      boy: "/boys-clothing/favorites/limited-time-sale"
+      girl: "/toddler-girls-clothes/",
+      boy: "/toddler-boys-clothes/"
     },
     sale: {
       girl: "/sale/girls-clothing/",
