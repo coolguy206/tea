503e0b02a48d5b095985a0cc98d1faee0b40c77a
coolguy206 <davidthanphilom@msn.com>
2022-12-15 14:17:19 -0800

    dropbox.js ftp.js promos/ homepage/ changed promo hero img master

--- dropbox.js
+++ dropbox.js
@@ -15,7 +15,7 @@ refreshAccessToken((data) => {
     accessToken: access_token
   });
 
-  var folderPath = `/Sam Khieu’s files/Shared/Design/2022/Promotions/12 December/121422 Winter Wonders/04 Handoff/Site/1214 PJs/`;
+  var folderPath = `/Sam Khieu’s files/Shared/Design/2022/Promotions/12 December/121422 Winter Wonders/04 Handoff/Site/1215/`;
   // var folderPath = `/Sam Khieu’s files/Shared/Design/2022/Campaign/120522 Small But Mighty Sweeps/04 Handoff/Site/`;
   folderPath = folderPath.toLowerCase();
   // console.log(folderPath);
