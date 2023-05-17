c3922ee3e5157117b1056582eea78e0c800ad8c3
David Thanphilom <davidthanphilom@msn.com>
2023-05-17 11:09:21 -0700

    gruntConfig.js content-zone/mega-menu/sub-nav added clearance to sale master

--- gruntConfig.js
+++ gruntConfig.js
@@ -37,7 +37,7 @@ module.exports = {
     name: 'every-mom'
   },
   zone: {
-    name: 'mega-menu/top-nav',
+    name: 'mega-menu/sub-nav',
     ftp: {
       css: {
         src: 'mega-menu/top-nav/css/',
