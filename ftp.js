865b91ad47fa46ca9f75cfe3e7b1356d38a8d04e
David Thanphilom <davidthanphilom@msn.com>
2023-11-07 16:26:33 -0800

    ftp.js mega-menu/ promos/default/ updated promo master

--- ftp.js
+++ ftp.js
@@ -7,22 +7,22 @@ require('dotenv').config()
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
-// var category = `promos/one-offs/2023/1105/v1/select-styles/`;
+var category = `promos/one-offs/2023/1107/v0/`;
 // var category = `homepage/2023/1102/v0/`;
-var category = `landing-pages/gift-shop/2023/1106/v0/`;
+// var category = `landing-pages/gift-shop/2023/1106/v0/`;
 // var category = `mega-menu/2023/0420/v0/`;
 
 //!EXCEPT THIS ONE
 var url = `${baseURL}${category}`
 
 //!NEEDS ENDING SLASH
-// var readFilePath = `Site/1105/04 Handoff/Site/family matching/category hero/`;
-var readFilePath = `handoff/landing-pages/gift-shop/110223 Gift Guide LP/Handoff/`;
+var readFilePath = `Site/1107/`;
+// var readFilePath = `handoff/landing-pages/gift-shop/110223 Gift Guide LP/Handoff/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
-// var writeFilePath = `promos/default/dev/images/homepage/`;
+var writeFilePath = `promos/default/dev/images/mega-menu/`;
 // var writeFilePath = `promos/headers/dev/images/select-styles/`;
-var writeFilePath = `landing-page/shops/gift-shop/default/dev/images/`;
+// var writeFilePath = `landing-page/shops/gift-shop/default/dev/images/`;
 // var writeFilePath = `homepage/default/dev/images/`;
 // var writeFilePath = `mega-menu/dev/images/`;
 var fileName = `c2b-m`;
