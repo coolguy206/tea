81ebcabc08ba1eaadd3531f8c95ea8790d54996d
David Thanphilom <davidthanphilom@msn.com>
2023-11-15 10:38:08 -0800

    landing-page/affiliate/careers removed director of marketing role master

--- landing-page/affiliate/careers/js/babel/job-listing.js
+++ landing-page/affiliate/careers/js/babel/job-listing.js
@@ -5,13 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.jobs = void 0;
 
-var _directorMarketing = require("./director-marketing.js");
-
 var _officeCoordinator = require("./office-coordinator.js");
 
 // import {
 //   digitalMarketingCoordinator
 // } from './digital-marketing-coordinator.js';
-var jobs = [_directorMarketing.directorMarketing, _officeCoordinator.officeCoordinator];
+// import {
+//   directorMarketing
+// } from './director-marketing.js';
+var jobs = [_officeCoordinator.officeCoordinator];
 exports.jobs = jobs;
 //# sourceMappingURL=job-listing.js.map
--- landing-page/affiliate/careers/js/job-listing.js
+++ landing-page/affiliate/careers/js/job-listing.js
@@ -1,13 +1,13 @@
 // import {
 //   digitalMarketingCoordinator
 // } from './digital-marketing-coordinator.js';
-import {
-  directorMarketing
-} from './director-marketing.js';
+// import {
+//   directorMarketing
+// } from './director-marketing.js';
 import {
   officeCoordinator
 } from './office-coordinator.js';
 
-export var jobs = [directorMarketing, officeCoordinator];
+export var jobs = [officeCoordinator];
 
 
