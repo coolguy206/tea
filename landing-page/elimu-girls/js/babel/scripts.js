86b670bdbfabf13ecdbbca0ad4db6ccefd7d8bcd
David Thanphilom <davidthanphilom@msn.com>
2024-01-31 12:38:06 -0800

    pulled from 01.30.landing.elimu-girls  dropbox.js ftp.js gruntConfig.js landing-page/elimu-girls/ master

---
+++ landing-page/elimu-girls/js/babel/scripts.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _inview = require("./inview.js");
+
+$(document).ready(function () {
+  (0, _inview.inview)('.container .c1, .container .c2,.container .c3');
+});
+//# sourceMappingURL=scripts.js.map
---
+++ landing-page/elimu-girls/js/scripts.js
@@ -0,0 +1,10 @@
+
+import {
+  inview
+} from './inview.js';
+
+$(document).ready(function() {
+
+  inview('.container .c1, .container .c2,.container .c3');
+
+});
