c69fb542edff8013aad9a25a5f703407f2af861b
David Thanphilom <davidthanphilom@msn.com>
2024-02-06 12:23:41 -0800

    landing-page/shops/swim-shop/ cleaned up files master

--- landing-page/shops/swim-shop/default/js/babel/sliders.js
+++
@@ -1,6 +0,0 @@
-// $('.hp .c1').bxSlider({
-//   infiniteLoop: true,
-//   auto: true
-// });
-"use strict";
-//# sourceMappingURL=sliders.js.map
--- landing-page/shops/swim-shop/default/js/scripts.js
+++ landing-page/shops/swim-shop/default/js/scripts.js
@@ -1,6 +1,4 @@
-import {
-  alt
-} from './alt.js';
+
 import {
   inview
 } from './inview.js';
@@ -18,29 +16,6 @@ $(document).ready(function() {
     gap:0
   }).mount()
 
-
-  alt('.dresses .c1 img', 'kids in pattern swimwear');
-  alt('.dresses .c2 .first img', 'girl in flower twirl dress');
-  alt('.dresses .c2 .second img', 'girl in fish graphic empire dress');
-  alt('.dresses .c2 .third img', 'girl in floral graphic wrap neck dress');
-  alt('.dresses .clothes .first img', 'one pieces');
-  alt('.dresses .clothes .second img', 'two pieces');
-  alt('.dresses .clothes .third img', 'rash guards');
-  alt('.dresses .clothes .fourth img', 'swim trunks');
-  alt('.dresses .clothes .fifth img', 'baby swim');
-  alt('.dresses .clothes .sixth img', 'famiily swim');
-  alt('.dresses .c3 .first img', 'girl in stripe graphic short sleeve dress');
-  alt('.dresses .c3 .second img', 'girl in flower graphic play dress');
-  alt('.dresses .c3 .third img', 'girl in special occasion dress');
-  alt('.dresses .c4 .first img', 'girl in floral graphic long sleeve dress');
-  alt('.dresses .c4 .second img', 'girl leggings');
-  alt('.dresses .c5 .first img', 'girl leggings');
-  alt('.dresses .leggings img', 'family swim');
-  alt('.dresses .graphics .first img', 'floral prints');
-  alt('.dresses .graphics .second img', 'animal prints');
-  alt('.dresses .graphics .third img', 'fruit prints');
-  alt('.dresses .graphics .fourth img', 'rainbow & stars prints');
-
-  inview('.dresses .c1, .dresses .c2, .dresses .c3, .dresses .c4, .dresses .c5, .dresses .c6, .dresses .c7, .dresses .clothes, .dresses .graphics, .dresses .dress-type, .dresses .slideshow, .dresses .reviews, .dresses .leggings');
+  inview('.swim .c1, .swim .c2, .swim .c3, .swim .c4, .swim .c5, .swim .c6, .swim .c7, .swim .clothes, .swim .graphics, .swim .dress-type, .swim .slideshow, .swim .reviews, .swim .leggings');
 
 });
