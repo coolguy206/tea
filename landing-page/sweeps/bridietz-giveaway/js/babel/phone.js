c0814015b9d145489571ffb002f5f4d4b4a1182b
David Thanphilom <davidthanphilom@msn.com>
2024-07-08 13:57:13 -0700

    landing-page/sweeps/bridietz-giveaway coded 07.08.landing.thetravelingchild-giveaway

---
+++ landing-page/sweeps/bridietz-giveaway/js/babel/phone.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.phoneConvert = void 0;
+var phoneConvert = exports.phoneConvert = function phoneConvert(string) {
+  var newString = string.match(/[0-9]{0,14}/g);
+  if (newString === null) {
+    return '';
+  }
+
+  // Join parts returned from RegEx match
+  newString = newString.join('');
+
+  // Start number with "+"
+  // newString = '+' + newString;
+
+  if (newString[0].includes('1')) {
+    newString = '+' + newString;
+  } else {
+    newString = '+1' + newString;
+  }
+
+  // Limit length to 15 characters
+  newString = newString.substring(0, 15);
+  return newString;
+};
+//# sourceMappingURL=phone.js.map
