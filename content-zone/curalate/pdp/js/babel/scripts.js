b24d6fdaef9c75d6b7ddb906b38d1fbc4fa38b89
David Thanphilom <davidthanphilom@msn.com>
2024-07-22 12:06:01 -0700

    content-zone/curalate/pdp/ fixed script master

--- content-zone/curalate/pdp/js/babel/scripts.js
+++ content-zone/curalate/pdp/js/babel/scripts.js
@@ -1,5 +1,10 @@
 "use strict";
 
-var sku = $('.product-page').attr('data-tea-sku').split(',')[0];
-$('div[data-crl8-container-id="product" ]').attr('data-crl8-filter', 'productId:' + sku);
+$(document).ready(function () {
+  var sku = $('.product-page').attr('data-tea-sku');
+  if (sku !== undefined && sku !== "") {
+    sku = sku.split(',')[0];
+    $('div[data-crl8-container-id="product" ]').attr('data-crl8-filter', 'productId:' + sku);
+  }
+});
 //# sourceMappingURL=scripts.js.map
--- content-zone/curalate/pdp/js/scripts.js
+++ content-zone/curalate/pdp/js/scripts.js
@@ -1,2 +1,7 @@
-var sku = $('.product-page').attr('data-tea-sku').split(',')[0];
-$('div[data-crl8-container-id="product" ]').attr('data-crl8-filter', 'productId:' + sku);
+$(document).ready(function () {
+    var sku = $('.product-page').attr('data-tea-sku')
+    if (sku !== undefined && sku !== "") {
+        sku = sku.split(',')[0];
+        $('div[data-crl8-container-id="product" ]').attr('data-crl8-filter', 'productId:' + sku);
+    }
+});
