03ff3c15884aa646d82221d3cb52716f1fdbdb08
David Thanphilom <davidthanphilom@msn.com>
2024-07-29 12:16:01 -0700

    homepage/ updated shop slideshow master

--- homepage/default/js/shop-slideshow/pdp.js
+++ homepage/default/js/shop-slideshow/pdp.js
@@ -1,9 +1,9 @@
 const pdps = [
 
     // { url: `https://www.teacollection.com/product/24f12301/long-sleeve-twirl-dress.html#turkish%20coastal%20floral`, num: 1 },
-    { url: ` https://www.teacollection.com/product/24f12110/unicorn-garden-graphic-tee.html#salmon `, num: 2 },
+    // { url: ` https://www.teacollection.com/product/24f12110/unicorn-garden-graphic-tee.html#salmon `, num: 2 },
     // { url: `https://www.teacollection.com/product/24f12210/flare-for-fun-pocket-pants.html#cassis`, num: 3 },
-    // { url: `https://www.teacollection.com/product/24f22114/boji-train-graphic-tee.html#blue%20yarrow`, num: 4 },
+    { url: `https://www.teacollection.com/product/24f22114/boji-train-graphic-tee.html#blue%20yarrow`, num: 4 },
     // { url: `https://www.teacollection.com/product/24f22203/playwear-pants.html#raw%20umber`, num: 5 },
     // { url: `https://www.teacollection.com/product/24f22119/long-sleeve-printed-pocket-tee.html#school%20of%20anchovies`, num: 6 },
     // { url: `https://www.teacollection.com/product/24f23200/timeless-stretch-twill-pants.html#silver%20pine`, num: 7 },
