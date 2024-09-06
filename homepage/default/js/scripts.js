5c03fd40645759d5aa6835fb7fed1b4158c2ff3f
David Thanphilom <davidthanphilom@msn.com>
2024-09-05 17:38:25 -0700

    homepage/ updated js master

--- homepage/default/js/scripts.js
+++ homepage/default/js/scripts.js
@@ -25,18 +25,18 @@ $(document).ready(function () {
 
   new Glide('.c1 .glide', {
     type: 'carousel',
-    // autoplay: 4000,
+    autoplay: 4000,
     animationDuration: 1000,
     perView: 1,
     hoverpause: true,
     gap: 0,
-    swipeThreshold: false,
-    dragThreshold: false,
+    // swipeThreshold: false,
+    // dragThreshold: false,
   }).mount();
 
   new Glide('.c2 .glide', {
     type: 'carousel',
-    // autoplay: 4000,
+    autoplay: 4000,
     animationDuration: 1000,
     perView: 1,
     hoverpause: true,
