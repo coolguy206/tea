cf23bdd17f2c75ae1f4b04c1506270b7186cd8d6
David Thanphilom <davidthanphilom@msn.com>
2024-10-16 12:50:28 -0700

    content-zone/hp-sliders/ coded 07.24.best.seller.zone

---
+++ content-zone/hp-sliders/new-arrivals-slider/container/js/babel/scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.new-arrivals-slider.boy').hide();
+  $('.the-gender span').on('click', function () {
+    var str = $(this).text().toLowerCase();
+    $('.the-gender span').removeClass('selected');
+    if (str == "girl") {
+      $('.new-arrivals-slider.girl').show();
+      $(this).addClass('selected');
+      $('.new-arrivals-slider.boy').hide();
+    } else {
+      $('.new-arrivals-slider.girl').hide();
+      $('.new-arrivals-slider.boy').show();
+      $(this).addClass('selected');
+    }
+  });
+});
+//# sourceMappingURL=scripts.js.map
