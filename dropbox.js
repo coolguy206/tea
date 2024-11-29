8b34154a2b2595ebaeb0f2a9ae83ea9318a073f9
David Thanphilom <davidthanphilom@msn.com>
2024-11-29 15:01:04 -0800

    dropbox.js ftp.js assets ready 12.03.giving-tues

--- dropbox.js
+++ dropbox.js
@@ -15,13 +15,13 @@ refreshAccessToken((data) => {
     accessToken: access_token
   });
 
-  var folderPath = `/Sam Khieu’s files/Shared/Design/2024/Promotions/11 November/113024 Cyber Monday/03 Handoff/Site/`;
+  // var folderPath = `/Sam Khieu’s files/Shared/Design/2024/Promotions/11 November/113024 Cyber Monday/03 Handoff/Site/`;
   // var folderPath = `/Sam Khieu’s files/Shared/Design/2024/Site/110724 Winter 2 HP/03 Handoff/`;
-  // var folderPath = `/Sam Khieu’s files/Shared/Design/2024/Campaign/101324 Parabo Giveaway/04 Handoff/LP/`
+  var folderPath = `/Sam Khieu’s files/Shared/Design/2024/Campaign/120324 Giving Tuesday/04 Handoff/Site/`
   folderPath = folderPath.toLowerCase();
   // console.log(folderPath);
 
-  var writeFilePath = `Site/1130/`;
+  var writeFilePath = `Site/1203/`;
   // var writeFilePath = `handoff/landing-pages/gift-guide/1107/`;
   writeFilePath = resolve(writeFilePath);
   // console.log(writeFilePath);
