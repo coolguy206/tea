3a34182e322634b0f07cb5a514a6c732ec229e81
David Thanphilom <davidthanphilom@msn.com>
2024-11-29 16:13:49 -0800

    homepage/ promos/ need urls 12.03.giving-tues

--- homepage/default/js/babel/scripts.js
+++ homepage/default/js/babel/scripts.js
@@ -6,26 +6,11 @@ var _glide = _interopRequireDefault(require("@glidejs/glide"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 $(document).ajaxComplete(function (event, xhr, options) {
   // console.log(event,xhr,options)
-  // ajaxCount = ajaxCount + 1;
-  // console.log(ajaxCount);
 
-  // if (ajaxCount == 2) {
-  $('.hp .shop-slideshow').append($('.cm-slider.best-sellers'));
-  $('.hp .shop-slideshow2').append($('.cm-slider.layers'));
-  $('.hp .shop-slideshow3').append($('.cm-slider.pants'));
+  $('.hp .shop-slideshow').append($('.the-sliders'));
+  // $('.hp .shop-slideshow2').append($('.cm-slider.layers'));
+  // $('.hp .shop-slideshow3').append($('.cm-slider.pants'));
   $('.hp .white-out').fadeOut();
-
-  // var dressesImg1 = `<li><a href="/black-friday/shop-all/dresses "><img src="/mas_assets/media/tea_collection/promos/black-friday/2024/1120/hp/v0/Dresses-01.jpg"></a></li>`;
-
-  // var dressesImg2 = `<li><a href="/black-friday/shop-all/dresses "><img src="/mas_assets/media/tea_collection/promos/black-friday/2024/1120/hp/v0/Dresses-02.jpg"></a></li>`;
-
-  // $('.bf-slider.dresses .lgw-thumb.glide__slide--active').before(dressesImg1);
-  // $('.bf-slider.dresses .lgw-thumb.glide__slide--active').next().next().next().next().before(dressesImg2);
-
-  // $('.the-sliders').clone().appendTo('.hp .shop-slideshow')
-  // $('.the-sliders').show();
-  // $('.the-sliders').css('opacity', 1);
-  // }
 });
 $(document).ready(function () {
   // if (window.innerWidth < 431) {
@@ -40,21 +25,16 @@ $(document).ready(function () {
   //   );
   // });
 
-  // $('.hp .shop-slideshow').append($('.the-sliders'));
-  // $('.the-sliders').clone().appendTo('.hp .shop-slideshow')
-  // $('.the-sliders').show();
-  // $('.the-sliders').css('opacity', 1);
-
-  // new Glide('.c1 .glide', {
-  //   type: 'carousel',
-  //   autoplay: 4000,
-  //   animationDuration: 1000,
-  //   perView: 1,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   // swipeThreshold: false,
-  //   // dragThreshold: false,
-  // }).mount();
+  new _glide["default"]('.c1 .glide', {
+    type: 'carousel',
+    // autoplay: 4000,
+    animationDuration: 1000,
+    perView: 1,
+    hoverpause: true,
+    gap: 0
+    // swipeThreshold: false,
+    // dragThreshold: false,
+  }).mount();
 
   // new Glide('.c2 .glide', {
   //   type: 'carousel',
@@ -65,51 +45,6 @@ $(document).ready(function () {
   //   gap: 0
   // }).mount();
 
-  // new Glide('.shop-slideshow .glide', {
-  //   type: 'carousel',
-  //   // autoplay: 4000,
-  //   animationDuration: 500,
-  //   perView: 5,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   breakpoints: {
-  //     431: {
-  //       perView: 2,
-  //       perSwipe: '|',
-  //     },
-  //   }
-  // }).mount();
-
-  // new Glide('.shop-slideshow2 .glide', {
-  //   type: 'carousel',
-  //   // autoplay: 500,
-  //   animationDuration: 500,
-  //   perView: 5,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   breakpoints: {
-  //     431: {
-  //       perView: 2,
-  //       perSwipe: '|',
-  //     },
-  //   }
-  // }).mount();
-
-  // new Glide('.shop-slideshow3 .glide', {
-  //   type: 'carousel',
-  //   // autoplay: 500,
-  //   animationDuration: 500,
-  //   perView: 5,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   breakpoints: {
-  //     431: {
-  //       perView: 2,
-  //       perSwipe: '|',
-  //     },
-  //   }
-  // }).mount();
-
   // new Glide('.c6 .glide', {
   //   type: 'carousel',
   //   autoplay: 4000,
@@ -118,13 +53,6 @@ $(document).ready(function () {
   //   hoverpause: true,
   //   gap: 0
   // }).mount()
-
-  // var imgs = $('.memorial-day-shop').find('img');
-  // imgs.map(function (i, val) {
-  //   var href = $(val).closest('span').attr('data-href');
-  //   $(val).addClass('memorial-day').attr('data-href', href);
-  //   $(val).next().addClass('memorial-day').attr('data-alt', $(val).attr('alt')).attr('data-href', href);
-  // });
 
   // $('.furoshiki .see-details').fancybox({
   //   helpers: {
--- homepage/default/js/scripts.js
+++ homepage/default/js/scripts.js
@@ -7,30 +7,14 @@ import {
 } from './tracking.js';
 import Glide from '@glidejs/glide'
 
-
-
 $(document).ajaxComplete(function (event, xhr, options) {
   // console.log(event,xhr,options)
-  // ajaxCount = ajaxCount + 1;
-  // console.log(ajaxCount);
 
-  // if (ajaxCount == 2) {
-    $('.hp .shop-slideshow').append($('.cm-slider.best-sellers'));
-    $('.hp .shop-slideshow2').append($('.cm-slider.layers'));
-    $('.hp .shop-slideshow3').append($('.cm-slider.pants'));
-    $('.hp .white-out').fadeOut();
+  $('.hp .shop-slideshow').append($('.the-sliders'));
+  // $('.hp .shop-slideshow2').append($('.cm-slider.layers'));
+  // $('.hp .shop-slideshow3').append($('.cm-slider.pants'));
+  $('.hp .white-out').fadeOut();
 
-    // var dressesImg1 = `<li><a href="/black-friday/shop-all/dresses "><img src="/mas_assets/media/tea_collection/promos/black-friday/2024/1120/hp/v0/Dresses-01.jpg"></a></li>`;
-
-    // var dressesImg2 = `<li><a href="/black-friday/shop-all/dresses "><img src="/mas_assets/media/tea_collection/promos/black-friday/2024/1120/hp/v0/Dresses-02.jpg"></a></li>`;
-
-    // $('.bf-slider.dresses .lgw-thumb.glide__slide--active').before(dressesImg1);
-    // $('.bf-slider.dresses .lgw-thumb.glide__slide--active').next().next().next().next().before(dressesImg2);
-
-    // $('.the-sliders').clone().appendTo('.hp .shop-slideshow')
-    // $('.the-sliders').show();
-    // $('.the-sliders').css('opacity', 1);
-  // }
 });
 
 $(document).ready(function () {
@@ -43,25 +27,20 @@ $(document).ready(function () {
   // $("#countdown span").countdown("2024/11/29 23:59:59", function (event) {
   //   $(this).text(
   //     event.strftime('%-DD %HH %MM %SS')
-      // event.strftime('%-D %H:%M:%S')
+  // event.strftime('%-D %H:%M:%S')
   //   );
   // });
 
-  // $('.hp .shop-slideshow').append($('.the-sliders'));
-  // $('.the-sliders').clone().appendTo('.hp .shop-slideshow')
-  // $('.the-sliders').show();
-  // $('.the-sliders').css('opacity', 1);
-
-  // new Glide('.c1 .glide', {
-  //   type: 'carousel',
-  //   autoplay: 4000,
-  //   animationDuration: 1000,
-  //   perView: 1,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   // swipeThreshold: false,
-  //   // dragThreshold: false,
-  // }).mount();
+  new Glide('.c1 .glide', {
+    type: 'carousel',
+    // autoplay: 4000,
+    animationDuration: 1000,
+    perView: 1,
+    hoverpause: true,
+    gap: 0,
+    // swipeThreshold: false,
+    // dragThreshold: false,
+  }).mount();
 
   // new Glide('.c2 .glide', {
   //   type: 'carousel',
@@ -70,51 +49,6 @@ $(document).ready(function () {
   //   perView: 1,
   //   hoverpause: true,
   //   gap: 0
-  // }).mount();
-
-  // new Glide('.shop-slideshow .glide', {
-  //   type: 'carousel',
-  //   // autoplay: 4000,
-  //   animationDuration: 500,
-  //   perView: 5,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   breakpoints: {
-  //     431: {
-  //       perView: 2,
-  //       perSwipe: '|',
-  //     },
-  //   }
-  // }).mount();
-
-  // new Glide('.shop-slideshow2 .glide', {
-  //   type: 'carousel',
-  //   // autoplay: 500,
-  //   animationDuration: 500,
-  //   perView: 5,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   breakpoints: {
-  //     431: {
-  //       perView: 2,
-  //       perSwipe: '|',
-  //     },
-  //   }
-  // }).mount();
-
-  // new Glide('.shop-slideshow3 .glide', {
-  //   type: 'carousel',
-  //   // autoplay: 500,
-  //   animationDuration: 500,
-  //   perView: 5,
-  //   hoverpause: true,
-  //   gap: 0,
-  //   breakpoints: {
-  //     431: {
-  //       perView: 2,
-  //       perSwipe: '|',
-  //     },
-  //   }
   // }).mount();
 
   // new Glide('.c6 .glide', {
@@ -126,19 +60,11 @@ $(document).ready(function () {
   //   gap: 0
   // }).mount()
 
-  // var imgs = $('.memorial-day-shop').find('img');
-  // imgs.map(function (i, val) {
-  //   var href = $(val).closest('span').attr('data-href');
-  //   $(val).addClass('memorial-day').attr('data-href', href);
-  //   $(val).next().addClass('memorial-day').attr('data-alt', $(val).attr('alt')).attr('data-href', href);
-  // });
-
   // $('.furoshiki .see-details').fancybox({
   //   helpers: {
   //     overlay: null
   //   }
   // });
-
 
   inview('.hp .promo1-skinny, .hp .promo1, .hp .promo2, .hp .promo3, .hp .promos-2-cell, .hp .c1, .hp .c2, .hp .c3, .hp .c4, .hp .c5, .hp .c6, .hp .c7, .hp .c8, .hp .graphics, .hp .refer-friend, .hp .rewear, .hp .shop-slideshow, .hp .shop-slideshow2, .hp .shop-slideshow3, .hp .why-tea');
 
