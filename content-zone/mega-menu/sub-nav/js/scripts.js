0844c484c63d81fbe16c3d0c6c35fbf72630c741
David Thanphilom <davidthanphilom@msn.com>
2024-12-09 15:50:55 -0800

    content-zone/mega-menu/ added rewear popup back master

--- content-zone/mega-menu/sub-nav/js/scripts.js
+++ content-zone/mega-menu/sub-nav/js/scripts.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
   <img class="kidizen-close" src="/mas_assets/media/tea_collection/content-zone/email-sign-up/lightbox/2019/0724/v1/+.svg">
   <img class="tea-rewear-logo" src="/mas_assets/media/tea_collection/content-zone/mega-menu/sub-nav/2022/0125/v0/tea-rewear.png">
     <p>Heads up! You’re about to leave<br class="mobile"> Tea to shop,<br class="desktop"> sell, or trade in pre-<br class="mobile">loved Tea on Tea Rewear.</p>
-    <a id="tea-rewear-leave" class="a cta-border " href="https://teacollection.kidizen.com/?utm_source=Tea+Collection&utm_medium=website&utm_campaign=Tea+Rewear" target="_blank">take me there</a>
+    <!--<a id="tea-rewear-leave" class="a cta-border " href="https://teacollection.kidizen.com/?utm_source=Tea+Collection&utm_medium=website&utm_campaign=Tea+Rewear" target="_blank">take me there</a>-->
+    <a id="tea-rewear-leave" class="a cta-border " href="https://teacollection.treet.co/" target="_blank">take me there</a>
   </div>`;
   var elem = overlay + content;
 
