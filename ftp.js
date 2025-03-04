710b295929b8819830b04749f1bc198f6d092c6f
David Thanphilom <davidthanphilom@msn.com>
2025-03-04 13:13:05 -0800

    dropbox.js ftp.js homepage/ promos/ updated hero master

--- ftp.js
+++ ftp.js
@@ -10,7 +10,7 @@ var num = `0301`;
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
-var category = `promos/one-offs/${year}/${num}/v0/`;
+var category = `promos/one-offs/${year}/${num}/v1/`;
 // var category = `homepage/${year}/${num}/v0/`;
 // var category = `headers/${year}/${num}/pom-pom/v0/`;
 // var category = `landing-pages/special-occasion/${year}/${num}/v0/`;
@@ -20,13 +20,13 @@ var category = `promos/one-offs/${year}/${num}/v0/`;
 var url = `${baseURL}${category}`
 
 //!NEEDS ENDING SLASH
-var readFilePath = `Site/${num}/Site/visual nav/`;
+var readFilePath = `Site/${num}/v2/hp/`;
 // var readFilePath = `handoff/landing-pages/dresses/${num}/handoff/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
-// var writeFilePath = `promos/default/dev/images/homepage/${num}/`;
+var writeFilePath = `promos/default/dev/images/homepage/${num}/`;
 // var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
-var writeFilePath = `promos/headers/dev/images/select-styles/`;
+// var writeFilePath = `promos/headers/dev/images/select-styles/`;
 // var writeFilePath = `landing-page/shops/special-occasion-shop/default/dev/images/${num}/`;
 // var writeFilePath = `homepage/default/dev/images/${num}/`;
 // var writeFilePath = `mega-menu/dev/images/`;
