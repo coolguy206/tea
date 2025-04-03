8a5255bd09271cc975643efb1f0d78ad54e1ae6e
David Thanphilom <davidthanphilom@msn.com>
2025-04-02 17:37:23 -0700

    ftp.js promos/headers updated easter edit master

--- ftp.js
+++ ftp.js
@@ -6,14 +6,14 @@ const mkDir = util.promisify(fs.mkdir);
 require('dotenv').config()
 
 var year = `2025`;
-var num = `0404`;
+var num = `0402`;
 var version = `v0`;
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
-var category = `promos/one-offs/${year}/${num}/${version}/`;
+// var category = `promos/one-offs/${year}/${num}/${version}/`;
 // var category = `homepage/${year}/${num}/${version}/`;
-// var category = `headers/${year}/${num}/easter-shop/${version}/`;
+var category = `headers/${year}/${num}/easter-shop/${version}/`;
 // var category = `landing-pages/spring-break-escape/${year}/${num}/${version}/`;
 // var category = `mega-menu/${year}/${num}/${version}/`;
 
@@ -21,13 +21,13 @@ var category = `promos/one-offs/${year}/${num}/${version}/`;
 var url = `${baseURL}${category}`
 
 //!NEEDS ENDING SLASH
-var readFilePath = `Site/${num}/040425 easter/mega/`;
+var readFilePath = `Site/${num}/`;
 // var readFilePath = `handoff/landing-pages/dresses/${num}/handoff/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
 // var writeFilePath = `promos/default/dev/images/homepage/${num}/`;
-var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
-// var writeFilePath = `promos/headers/dev/images/landing-pages/easter-edit/`;
+// var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
+var writeFilePath = `promos/headers/dev/images/landing-pages/easter-edit/`;
 // var writeFilePath = `landing-page/sweeps/spring-break-escape/dev/images/${num}/`;
 // var writeFilePath = `homepage/default/dev/images/${num}/`;
 // var writeFilePath = `mega-menu/dev/images/`;
