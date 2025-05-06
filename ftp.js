ae0be1918563005f3dd3b5f0476f18763d60df42
David Thanphilom <davidthanphilom@msn.com>
2025-05-06 11:56:14 -0700

    dropbox.js ftp.js homepage/ HP coded 05.07.teatush

--- ftp.js
+++ ftp.js
@@ -6,13 +6,13 @@ const mkDir = util.promisify(fs.mkdir);
 require('dotenv').config()
 
 var year = `2025`;
-var num = `0427`;
+var num = `0507`;
 var version = `v0`;
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
-var category = `promos/one-offs/${year}/${num}/${version}/`;
-// var category = `homepage/${year}/${num}/${version}/`;
+// var category = `promos/one-offs/${year}/${num}/${version}/`;
+var category = `homepage/${year}/${num}/${version}/`;
 // var category = `headers/${year}/${num}/print-shop/${version}/`;
 // var category = `landing-pages/spring-break-escape/${year}/${num}/${version}/`;
 // var category = `mega-menu/${year}/${num}/${version}/`;
@@ -21,15 +21,15 @@ var category = `promos/one-offs/${year}/${num}/${version}/`;
 var url = `${baseURL}${category}`
 
 //!NEEDS ENDING SLASH
-var readFilePath = `Site/${num}/Site/headers/`;
+var readFilePath = `Site/${num}/Site/hp/`;
 // var readFilePath = `handoff/landing-pages/dresses/${num}/handoff/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
 // var writeFilePath = `promos/default/dev/images/homepage/${num}/`;
 // var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
-var writeFilePath = `promos/headers/dev/images/select-styles/`;
+// var writeFilePath = `promos/headers/dev/images/select-styles/`;
 // var writeFilePath = `landing-page/sweeps/spring-break-escape/dev/images/${num}/`;
-// var writeFilePath = `homepage/default/dev/images/${num}/`;
+var writeFilePath = `homepage/default/dev/images/${num}/`;
 // var writeFilePath = `mega-menu/dev/images/`;
 var fileName = `c2b-m`;
 var ext = `.jpg`;
