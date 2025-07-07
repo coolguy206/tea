4bae8f1bb63e2cdd6fee96f8f48097ca0dea93b4
David Thanphilom <davidthanphilom@msn.com>
2025-07-07 13:06:59 -0700

    grunt/ts.js added output master

--- grunt/ts.js
+++ grunt/ts.js
@@ -14,23 +14,28 @@ module.exports = {
 
     hp: {
         // tsconfig: './tsconfig.json'
-        src: ["homepage/" + config.hp.name + "/ts/**/*.ts", "!node_modules/**"]
+        src: ["homepage/" + config.hp.name + "/ts/**/*.ts", "!node_modules/**"],
+        outDir: "homepage/" + config.hp.name + "/js/jsmin",
     },
 
     mega: {
         src: ['mega-menu/ts/*.ts', "!node_modules/**"],
+        outDir
     },
 
     zone: {
         src: ['content-zone/' + config.zone.name + '/**/ts/*.ts'],
+        outDir: 'content-zone/' + config.zone.name + '/js/jsmin',
     },
 
     landing: {
         src: ['landing-page/' + config.landing.name + '/**/ts/**/*.ts'],
+        outDir: 'landing-page/' + config.landing.name + '/js/jsmin',
     },
 
     promos: {
         src: ['promos/' + config.promo.name + '/**/ts/**/*.ts'],
+        outDir: 'promos/' + config.promo.name + '/js/jsmin',
     },
 
 };
