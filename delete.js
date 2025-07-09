d6f02b0499c47498f7da43aa1450ad29faacd309
David Thanphilom <davidthanphilom@msn.com>
2025-07-09 15:13:09 -0700

    delete.js grunt/less.js edited delete.js to delete folders too and changed src and dest for less.js 07.11.revert

--- delete.js
+++ delete.js
@@ -5,38 +5,52 @@ const writeFileContent = util.promisify(fs.writeFile);
 const mkDir = util.promisify(fs.mkdir);
 
 //!NEEDS ENDING SLASH
-var readFilePath = `handoff/landing-pages/dresses/handoff/`;
+var readFilePath = `content-zone/mega-menu/sub-nav/less/`;
 
 fs.readdir(`${readFilePath}`, (err, files) => {
-    //   console.log(files);
-    files.map((file, i) => {
-        // console.log(file);
-        
-        if(file.indexOf(`v2`) !== -1){
-            // console.log(file);
-            try {
-                fs.unlinkSync(readFilePath + file)
-                console.log(`${file} has been deleted`);
-                
-              } catch(err) {
-                console.log(`oops something went wrong`);
-                console.error(err)
-              }
-        }
+  //   console.log(files);
+  files.map((file, i) => {
+    // console.log(file);
 
-        // fs.unlink()
+    //? delete folders
+    // if (file.indexOf(`toolkit`) !== -1) {
+    //   // console.log(file);
+    //   try {
+    //     fs.rmSync(readFilePath + file, { recursive: true })
+    //     console.log(`${file} has been deleted and all its contents`);
 
-        // fs.rename(readFilePath + file, readFilePath + newName, (err) => {
-        //     if(err){
-        //         console.log(`oops something went wrong`);
-        //         console.log(err);
-        //     } else {
-        //         console.log(`${file} renamed to ${newName}`);
-        //     }
-        
-        // });
+    //   } catch (err) {
+    //     console.log(`oops something went wrong`);
+    //     console.error(err)
+    //   }
+    // }
 
-    });
+    //? delete files
+    if (file.indexOf(`.css`) !== -1) {
+      console.log(file);
+      try {
+        fs.unlinkSync(readFilePath + file, { recursive: true, force: true })
+        console.log(`${file} has been deleted`);
+
+      } catch (err) {
+        console.log(`oops something went wrong`);
+        console.error(err)
+      }
+    }
+
+    // fs.unlink()
+
+    // fs.rename(readFilePath + file, readFilePath + newName, (err) => {
+    //     if(err){
+    //         console.log(`oops something went wrong`);
+    //         console.log(err);
+    //     } else {
+    //         console.log(`${file} renamed to ${newName}`);
+    //     }
+
+    // });
+
+  });
 
 });
 
--- grunt/less.js
+++ grunt/less.js
@@ -2,22 +2,27 @@ var config = require("./../gruntConfig.js");
 // console.log(config.landing);
 var promoCSS = config.promo.css;
 
+var promosSrc = ``;
 var promosDest = ``;
 if (config.promo.css !== ``) {
     if (promoCSS.indexOf('/') !== -1) {
         promoCSS = promoCSS.split('/');
         var slashCount = promoCSS.length;
         if (slashCount == 3) {
-            promosDest = `promos/${config.promo.name}/dev/${promoCSS[0]}/${promoCSS[1]}//${promoCSS[2]}/css/`;
+            promosSrc = `promos/${config.promo.name}/dev/${promoCSS[0]}/${promoCSS[1]}/${promoCSS[2]}/less/`;
+            promosDest = `promos/${config.promo.name}/dev/${promoCSS[0]}/${promoCSS[1]}/${promoCSS[2]}/css/`;
         } else {
+            promosSrc = `promos/${config.promo.name}/dev/${promoCSS[0]}/${promoCSS[1]}/less/`;
             promosDest = `promos/${config.promo.name}/dev/${promoCSS[0]}/${promoCSS[1]}/css/`;
         }
 
     } else {
+        promosSrc = `promos/${config.promo.name}/dev/${config.promo.css}/less/`;
         promosDest = `promos/${config.promo.name}/dev/${config.promo.css}/css/`;
     }
 
 } else {
+    promosSrc = `promos/${config.promo.name}/less/`
     promosDest = `promos/${config.promo.name}/css/`
 }
 // console.log(promosDest);
@@ -32,7 +37,7 @@ module.exports = {
 
     mega: {
         expand: true,
-        cwd: 'mega-menu/css/',
+        cwd: 'mega-menu/less/',
         src: '*.less',
         dest: 'mega-menu/css/',
         ext: '.css',
@@ -41,7 +46,7 @@ module.exports = {
 
     zone: {
         expand: true,
-        cwd: 'content-zone/' + config.zone.name + '/css/',
+        cwd: 'content-zone/' + config.zone.name + '/less/',
         src: '*.less',
         dest: 'content-zone/' + config.zone.name + '/css/',
         ext: '.css',
@@ -50,7 +55,7 @@ module.exports = {
 
     hp: {
         expand: true,
-        cwd: 'homepage/' + config.hp.name + '/css/',
+        cwd: 'homepage/' + config.hp.name + '/less/',
         src: '**/*.less',
         dest: 'homepage/' + config.hp.name + '/css/',
         ext: '.css',
@@ -60,7 +65,7 @@ module.exports = {
 
     categories: {
         expand: true,
-        cwd: 'category-pages/' + config.cat.name + '/css/',
+        cwd: 'category-pages/' + config.cat.name + '/less/',
         src: '*.less',
         dest: 'category-pages/' + config.cat.name + '/css/',
         ext: '.css',
@@ -68,7 +73,7 @@ module.exports = {
 
     landing: {
         expand: true,
-        cwd: 'landing-page/' + config.landing.name + '/css/',
+        cwd: 'landing-page/' + config.landing.name + '/less/',
         src: '*.less',
         dest: 'landing-page/' + config.landing.name + '/css/',
         ext: '.css',
@@ -77,7 +82,7 @@ module.exports = {
 
     promos: {
         expand: true,
-        cwd: promosDest,
+        cwd: promosSrc,
         src: ['**/*.less'],
         dest: promosDest,
         ext: '.css',
