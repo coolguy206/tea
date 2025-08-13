c627832d48bb169c1d40a04665178421a8dc1c8a
David Thanphilom <davidthanphilom@msn.com>
2025-08-13 16:45:44 -0700

    promos/ added another banner master

--- promos/default/js/babel/scripts.js
+++ promos/default/js/babel/scripts.js
@@ -96,18 +96,16 @@ jQuery(document).ready(function () {
     $.fancybox.close();
     $('.fancybox-container').hide();
   });
-
-  // $('.promos-wrap').bxSlider({
-  //   auto: true,
-  //   autoHover: true,
-  //   mode: 'fade',
-  //   controls: false,
-  //   pager: false,
-  //   // pause: 8000,
-  //   touchEnabled: false,
-
-  //   onSliderLoad: function () {
-  //     $('.promos.promo1, .promos.promo2, .promos.promo3').show();
-  //   }
-  // });
+  $('.promos-wrap').bxSlider({
+    auto: true,
+    autoHover: true,
+    mode: 'fade',
+    controls: false,
+    pager: false,
+    pause: 2000,
+    touchEnabled: false,
+    onSliderLoad: function onSliderLoad() {
+      $('.promos.promo1, .promos.promo2, .promos.promo3').show();
+    }
+  });
 });
