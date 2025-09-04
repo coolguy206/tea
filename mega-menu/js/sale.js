162ef5796477651f64869d8746d97ba700a64a86
David Thanphilom <davidthanphilom@msn.com>
2025-09-04 13:19:45 -0700

    mega-menu/ coded 09.07.promo

--- mega-menu/js/sale.js
+++ mega-menu/js/sale.js
@@ -17,9 +17,9 @@ $(document).ready(function () {
   $('.dept-girl').hover(function () {
     // $('.promo1-url, .promo2-url, .clearance-url').show();
     // $('.promo1-url').hide();
-    // $('.promo1-url.girl').show();
-    $('.promo1-url').show();
-    $('.promo2-url').show();
+    $('.promo1-url.girl').show();
+    // $('.promo1-url').show();
+    // $('.promo2-url').show();
     //  $('.promo3-url').show();
     //  $('.clearance-url').show();
     // $('.promo2-url').hide();
@@ -33,9 +33,9 @@ $(document).ready(function () {
   $('.dept-boy').hover(function () {
     // $('.promo1-url, .promo2-url, .clearance-url').show();
     // $('.promo1-url').hide();
-    // $('.promo1-url.boy').show();
-    $('.promo1-url').show();
-    $('.promo2-url').show();
+    $('.promo1-url.boy').show();
+    // $('.promo1-url').show();
+    // $('.promo2-url').show();
     // $('.promo3-url').show();
     // $('.clearance-url').show();
     // $('.promo2-url').hide();
@@ -48,9 +48,9 @@ $(document).ready(function () {
 
   $('.dept-sale').hover(function () {
 
-    // $('.promo1-url.girl').show();
-    $('.promo1-url').show();
-    $('.promo2-url').show();
+    $('.promo1-url.girl').show();
+    // $('.promo1-url').show();
+    // $('.promo2-url').show();
     // $('.promo3-url').show();
     // $('.clearance-url').show();
     // $('.promo1-url, .promo2-url, .promo3-url, .clearance-url').show();
@@ -69,9 +69,9 @@ $(document).ready(function () {
   $('.dept-swim, .dept-dress').hover(function () {
 
     // $('.promo1-url.swim').show();
-    // $('.promo1-url.girl').show();
-    $('.promo1-url').show();
-    $('.promo2-url').show();
+    $('.promo1-url.girl').show();
+    // $('.promo1-url').show();
+    // $('.promo2-url').show();
     // $('.promo3-url').show();
     // $('.clearance-url').show();
     // $('.promo1-url, .promo2-url, .clearance-url').show();
@@ -86,14 +86,14 @@ $(document).ready(function () {
 
   $('.dept-baby').hover(function () {
 
-    $('.promo1-url').show();
-    $('.promo2-url').show();
+    $('.promo1-url.baby-girl').show();
+    // $('.promo1-url.baby-boy').show();
+    // $('.promo1-url').show();
+    // $('.promo2-url').show();
     // $('.promo3-url').show();
     // $('.clearance-url').show();
     // $('.promo1-url, .promo2-url, .clearance-url').show();
     // $('.promo1-url').hide();
-    // $('.promo1-url.baby-girl').show();
-    // $('.promo1-url.baby-boy').show();
     // $('.promo2-url').hide();
     // $('.clearance-url').hide();
     // $('.promo1-url, .promo2-url, .promo3-url, .clearance-url').hide();
