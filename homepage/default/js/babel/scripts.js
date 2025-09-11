87163232ed335588358b6ebfa293c617bd7f76c4
David Thanphilom <davidthanphilom@msn.com>
2025-09-10 17:43:16 -0700

    homepage/ promos/ coded 09.15.promo

--- homepage/default/js/babel/scripts.js
+++ homepage/default/js/babel/scripts.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
 
   new _glide["default"]('.c1 .glide', {
     type: 'carousel',
-    // autoplay: 4000,
+    autoplay: 4000,
     animationDuration: 1000,
     perView: 1,
     hoverpause: true,
