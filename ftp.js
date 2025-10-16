b7e4a9840e48bc602f90c386cb64891930c3b5ec
David Thanphilom <davidthanphilom@msn.com>
2025-10-16 15:58:41 -0700

    ftp.js promos/ assets ready 10.20.promo

--- ftp.js
+++ ftp.js
@@ -6,15 +6,15 @@ const mkDir = util.promisify(fs.mkdir);
 require('dotenv').config()
 
 var year = `2025`;
-var num = `1012`;
+var num = `1020`;
 var version = `v0`;
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
-// var category = `promos/one-offs/${year}/${num}/${version}/`;
+var category = `promos/one-offs/${year}/${num}/${version}/`;
 // var category = `homepage/${year}/${num}/${version}/`;
 // var category = `headers/${year}/${num}/${version}/`;
-var category = `landing-pages/wallshoppe/${year}/${num}/${version}/`;
+// var category = `landing-pages/wallshoppe/${year}/${num}/${version}/`;
 // var category = `mega-menu/${year}/${num}/${version}/`;
 
 //!EXCEPT THIS ONE
@@ -28,9 +28,9 @@ var readFilePath = `Site/${num}/Site/mega/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
 // var writeFilePath = `promos/default/dev/images/homepage/${num}/`;
-// var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
+var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
 // var writeFilePath = `promos/headers/dev/images/promo/gift-save/`;
-var writeFilePath = `landing-page/sweeps/wallshoppe-giveaway/dev/images/`;
+// var writeFilePath = `landing-page/sweeps/wallshoppe-giveaway/dev/images/`;
 // var writeFilePath = `homepage/default/dev/images/${num}/`;
 // var writeFilePath = `mega-menu/dev/images/`;
 var fileName = `c2b-m`;
