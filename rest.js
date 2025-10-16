509a3e5f2943ebc16aafc5013a71bc86e182267c
David Thanphilom <davidthanphilom@msn.com>
2025-10-16 15:49:58 -0700

    rest.js fixed master

--- rest.js
+++ rest.js
@@ -1,258 +1,136 @@
 const fs = require('fs');
 const util = require('util');
 const writeFilePromise = util.promisify(fs.writeFile);
-const resolve = require('path').resolve
-require('dotenv').config()
+const resolve = require('path').resolve;
+require('dotenv').config();
 const moment = require('moment');
 const Client = require('ftp');
 const schedule = require('node-schedule');
 
-// console.log(`TEA_REST_API_USERNAME: ${process.env.TEA_REST_API_USERNAME}`);
-// console.log(`TEA_REST_API_PASSWORD: ${process.env.TEA_REST_API_PASSWORD}`);
-
-//? REST AUTH
-let auth = `${process.env.TEA_REST_API_USERNAME}:${process.env.TEA_REST_API_PASSWORD}`;
-// console.log(auth)
-
-auth = Buffer.from(auth).toString('base64');
-auth = `Basic ${auth}`;
-// console.log('Base64 Encoded:', auth);
-
-
-//? RUN THE SCHEDULE TO EXECUTE REST.JS EVERY HOUR
-// const job = schedule.scheduleJob('0 * * * *', function () {
-console.log(`running scheduled task to update pdps-order-lines.js`);
-
-//? GET TODAY'S DATE AND TIME BUT 3HRS BEFORE UNLESS IT IS 12AM 
-//? THEN CHANGE THE DATE TO THE DAY BEFORE AND CHANGE THE TIME TO LIKE 10PM
-let today = new Date();
-let hours = today.getHours();
-let day = today.getDate();
-if (hours < 3) {
-    day = day - 1;
-    today.setDate(day);
-    hours = 23 - 3;
-} else {
-    hours = hours - 3;
+//? Convert fetch requests into async functions
+async function fetchData(url, options) {
+    const response = await fetch(url, options);
+    return response.json();
 }
-today.setHours(hours);
 
-//? FOR TESTING
-// today = 1753252235350;
+//? Schedule job to run every hour
+const job = schedule.scheduleJob('0 * * * *', async function () {
+    console.log(`running scheduled task to update pdps-order-lines.js`);
 
-//? CONVERT TO UNIX TIME
-today = moment(today).unix();
-// console.log(`today: ${today}`);
+    //? Calculate the date/time 3 hours before or set to 10PM previous day
+    let today = new Date();
+    let hours = today.getHours();
+    let day = today.getDate();
 
-//? GOT THIS FROM POSTMAN BUT AUTH IS BASIC EMAIL:PASSWORD USING BASE64 ENCODE
-const myHeaders = new Headers();
-myHeaders.append("Authorization", auth);
-myHeaders.append("Cookie", "cart_marker=6c6f10bb4e3b0951317d587e74902bf509b1f150; ds3=a02404bd65aa5b2e4fd25ed9770b8220");
+    if (hours < 3) {
+        day -= 1; //? Subtract a day if before 3AM
+        today.setDate(day);
+        today.setHours(22, 0, 0, 0); //? Set to 10PM previous day
+    } else {
+        today.setHours(hours - 3, 0, 0, 0); //? Subtract 3 hours
+    }
 
-const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow"
-};
+    //? Convert to UNIX timestamp
+    console.log(`today: ${today}`);
+    today = moment(today).unix();
+    console.log(`today: ${today}`);
 
-//? GET THE PDPS ORDER FROM TODAY
-fetch(`https://www.teacollection.com/?type=rest&page=order_lines&format=json&created:after=${today}`, requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-        // console.log(result);
+    //? Prepare headers with Basic Auth
+    const auth = Buffer.from(`${process.env.TEA_REST_API_USERNAME}:${process.env.TEA_REST_API_PASSWORD}`).toString('base64');
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Basic ${auth}`);
 
-        //? CHECK IF THERE ARE ORDERS OTHERWISE JUST RETURN
-        if (result.order_lines !== undefined) {
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+    };
 
-            //? SKUS ARRAY
-            let skuArray = [];
+    //? Fetch order lines created after the calculated date
+    const result = await fetchData(`https://www.teacollection.com/?type=rest&page=order_lines&format=json&created:after=${today}`, requestOptions);
 
-            //? CONVERT INTO ARRAY OF OBJS
-            const orderLinesArray = Object.values(result.order_lines).filter(item => typeof item === 'object');
-            // console.log(orderLinesArray)
+    if (result.order_lines !== undefined) {
+        let skuArray = [];
+        const orderLinesArray = Object.values(result.order_lines).filter(item => typeof item === 'object');
 
-            //? PUSH INTO ARRAY
-            orderLinesArray.map((val, i) => {
-                // console.log(val.sku);
-                let sku = val.sku;
-                sku = sku.split('-')[0];
-                skuArray.push(sku);
-            });
+        //? Process order lines to extract SKUs
+        orderLinesArray.forEach(val => {
+            let sku = val.sku.split('-')[0];
+            skuArray.push(sku);
+        });
 
-            //? REMOVE DUPLICATES
-            const uniqueArray = [...new Set(skuArray)];
-            // console.log(uniqueArray);
+        const uniqueArray = [...new Set(skuArray)]; //? Remove duplicates
 
-            return uniqueArray;
-        } else {
-            console.log(`no purchases`);
-            return;
+        //? Fetch PDP info for each SKU
+        const pdpResults = await Promise.all(uniqueArray.map(async (val) => {
+            const response = await fetch(`https://www.teacollection.com/?type=rest&full=1&page=items&format=json&sku:begins=${val}&active=1`, requestOptions);
+            const result = await response.json();
+            const resultArray = Object.values(result.items).filter(item => typeof item === 'object');
+            return resultArray[0]; //? Return first item for each SKU because of variants
+        }));
+
+        //? Process PDP results
+        const arr = [];
+
+        for (const val of pdpResults) {
+            //? sku must contain a hyphen because otherwise it is a parent sku & DON'T ADD THE GIFT CARD PURCHASES
+            if (val.sku.includes('-') && !val.sku.includes('GIFTFUROSERV')) {
+                const obj = {};
+
+                const response = await fetch(`https://teacollection.com/?type=bare&page=item&action=images&id=${val.sku}`);
+                const imgSrcs = await response.json();
+
+                if (imgSrcs[0]) {
+                    obj.img = `<img src="${imgSrcs[0]}" alt="${val.model}">`;
+                }
+
+                obj.sku = val.sku;
+                obj.name = val.model;
+
+                //? Build URL based on SKU and model
+                let theSku = val.sku.split('-')[0];
+                let theName = val.model.replace(/ /g, '-').toLowerCase();
+
+                if (val.color !== undefined) {
+                    obj.color = val.color;
+                    obj.url = `https://www.teacollection.com/product/${theSku}/${theName}.html#${obj.color}`;
+                } else {
+                    obj.url = `https://www.teacollection.com/product/${theSku}/${theName}.html`;
+                }
+
+                //? Store prices
+                if (val.store_price !== val.retail_price) {
+                    obj.store_price = `$${Number(val.store_price).toFixed(2)}`;
+                    obj.retail_price = `$${Number(val.retail_price).toFixed(2)}`;
+                } else {
+                    obj.store_price = `$${Number(val.store_price).toFixed(2)}`;
+                }
+
+                if (val.promo_teaser !== "") {
+                    obj.promo_teaser = val.promo_teaser;
+                }
+
+                arr.push(obj);
+
+            }
         }
 
-    }).then((arr) => {
+        const json = JSON.stringify(arr);
+        await writeFilePromise('pdps-order-lines.js', json);
+        console.log('successfully created pdps-order-lines.js');
 
-        //? CHECK IF THERE IS AN ARRAY OTHERWISE STOP THE SCRIPT
-        if (arr !== undefined) {
-
-            //? GET ALL THE PDP INFO FROM THE ARRAY OF SKUS
-            Promise.all(arr.map((val, i) => {
-                // console.log(val);
-
-                const myHeaders = new Headers();
-                myHeaders.append("Authorization", auth);
-                myHeaders.append("Cookie", "cart_marker=6c6f10bb4e3b0951317d587e74902bf509b1f150; ds3=a02404bd65aa5b2e4fd25ed9770b8220");
-
-                const requestOptions = {
-                    method: "GET",
-                    headers: myHeaders,
-                    redirect: "follow"
-                };
-
-                return fetch(`https://www.teacollection.com/?type=rest&full=1&page=items&format=json&sku:begins=${val}&active=1`, requestOptions)
-                    .then((response) => response.json())
-                    .then((result) => {
-                        // console.log(result)
-
-                        //? CONVERT INTO ARRAY OF OBJS
-                        const resultArray = Object.values(result.items).filter(item => typeof item === 'object');
-                        // console.log(resultArray[0]);
-
-                        //? RETURN ONLY THE 1ST ONE BECAUSE ITS ONLY DIFFERENT SIZES
-                        return resultArray[0];
-                    })
-                    .catch((error) => console.error(error));
-
-            })).then((result) => {
-                // console.log(result);
-
-                //? MAKE ARRAY OF PDP OBJS RETURNED FROM THE FETCH TO WRITE TO FILE AND UPLOAD FTP
-                //? imgSrcHtml IS HTML UPLOADED TO FTP AND INCLUDED USING BANNER ADS TO GET THE IMG SRC
-                let arr = [];
-                let imgSrcHtml = ``;
-
-                result.map((val, i) => {
-
-                    //? DON'T ADD THE GIFT CARD PURCHASES THOUGH
-                    let theSku = val.sku;
-                    if (theSku.indexOf('GIFTFUROSERV') == -1) {
-                        let obj = {};
-                        obj.sku = val.sku;
-                        obj.name = val.model;
-                        obj.img = `{image size="original" type="item" id="${val.sku}"}`;
-
-                        //? BUILD OUT TEXT FOR OBJ.URL
-                        let theSku = val.sku;
-                        theSku = theSku.split('-')[0];
-                        let theName = val.model;
-                        theName = theName.replace(/ /g, '-').toLowerCase();
-
-                        //? CHECK IF COLOR IS NOT UNDEFINED
-                        if (val.color !== undefined) {
-                            obj.color = val.color;
-                            obj.url = `https://www.teacollection.com/product/${theSku}/${theName}.html#${obj.color}`;
-                        } else {
-                            obj.url = `https://www.teacollection.com/product/${theSku}/${theName}.html`;
-                        }
-
-                        //? STORE PRICE VS RETAIL PRICE
-                        if (val.store_price !== val.retail_price) {
-                            obj.store_price = `$${Number(val.store_price).toFixed(2)}`;
-                            obj.retail_price = `$${Number(val.retail_price).toFixed(2)}`;
-                        } else {
-                            obj.store_price = `$${Number(val.store_price).toFixed(2)}`;
-                        }
-
-                        //? CHECK IF PROMO_TEASER IS NOT UNDEFINED
-                        if (val.promo_teaser !== "") {
-                            obj.promo_teaser = val.promo_teaser;
-                        }
-
-                        //? PUSH TO ARRAY
-                        arr.push(obj);
-
-                        //? ADD TO HTML
-                        imgSrcHtml = `${imgSrcHtml}\n<div><span class="ref-url">${obj.url}</span><span class="ref-img">${obj.img}</span></div>\n`;
-                    }
-
-                });
-
-                //? ADD DIV CONTAINER IMG SRC
-                imgSrcHtml = `<div class="ref-html" style="display: none">${imgSrcHtml}</div>`;
-
-                // console.log(arr);
-                // console.log(imgSrcHtml);
-
-                //? CONVERT INTO STRING
-                let json = JSON.stringify(arr);
-
-                //? WRITE FILE AND UPLOAD
-                writeFilePromise('pdps-order-lines-img-src.html', imgSrcHtml).then(() => {
-                    console.log(`successfully created pdps-order-lines-img-src.html`);
-
-                    /*
-                    //? UPLOAD FILE FTP
-                    var url = `/media/tea_collection/js/`;
-
-                    var c = new Client();
-                    c.on('ready', function () {
-
-                        //? UPLOAD THE FILE
-                        c.put(`pdps-order-lines-img-src.html`, `${url}pdps-order-lines-img-src.html`, (err) => {
-                            if (err) throw err;
-                            console.log(`successfully uploaded file: pdps-order-lines-img-src.html`);
-
-                            c.end();
-                        });
-
-                    });
-
-                    c.connect({
-                        host: process.env.FTP_HOST,
-                        user: process.env.FTP_USER,
-                        password: process.env.FTP_PASSWORD
-                    });
-                    */
-                });
-
-
-                //? WRITE FILE AND UPLOAD
-                writeFilePromise('pdps-order-lines.js', json).then(() => {
-                    console.log('successfully created pdps-order-lines.js');
-
-                    /*
-                    //? UPLOAD FILE FTP
-                    var url = `/media/tea_collection/js/`;
-
-                    var c = new Client();
-                    c.on('ready', function () {
-
-                        //? UPLOAD THE FILE
-                        c.put(`pdps-order-lines.js`, `${url}pdps-order-lines.js`, (err) => {
-                            if (err) throw err;
-                            console.log(`successfully uploaded file: pdps-order-lines.js`);
-
-                            c.end();
-                        });
-
-                    });
-
-                    c.connect({
-                        host: process.env.FTP_HOST,
-                        user: process.env.FTP_USER,
-                        password: process.env.FTP_PASSWORD
-                    });
-                    */
-
-                }).catch((error) => console.error('Error writing file:', error));
-
+        //? Upload files via FTP
+        const ftpClient = new Client();
+        ftpClient.on('ready', () => {
+            ftpClient.put('pdps-order-lines.js', '/media/tea_collection/js/pdps-order-lines.js', (err) => {
+                if (err) throw err;
+                console.log('successfully uploaded pdps-order-lines.js');
+                ftpClient.end();
             });
-
-        } else {
-            console.log(`stop script`);
-            return;
-        }
-
-    }).catch((error) => console.error(error));
-
-
-// });
+        });
+        ftpClient.connect({ host: process.env.FTP_HOST, user: process.env.FTP_USER, password: process.env.FTP_PASSWORD });
+    } else {
+        console.log(`no purchases`);
+        return;
+    }
+});
