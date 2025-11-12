594eb78b2620ad339c09c3e61c917b7475d3cf25
David Thanphilom <davidthanphilom@msn.com>
2025-11-12 11:42:05 -0800

    ftp.js promos/ homepage/ coded 11.22.bf

--- ftp.js
+++ ftp.js
@@ -6,7 +6,7 @@ const mkDir = util.promisify(fs.mkdir);
 require('dotenv').config()
 
 var year = `2025`;
-var num = `1117`;
+var num = `1122`;
 var version = `v0`;
 
 //!NEEDS ENDING SLASH
@@ -22,7 +22,7 @@ var category = `promos/black-friday/${year}/${num}/${version}/`;
 var url = `${baseURL}${category}`
 
 //!NEEDS ENDING SLASH
-var readFilePath = `Site/${num}/Site/111725/hp/`;
+var readFilePath = `Site/${num}/Site/hp/`;
 // var readFilePath = `Site/${num}/Site/mega/`;
 // var readFilePath = `Site/${num}/mega/`;
 // var readFilePath = `handoff/landing-pages/dresses/${num}/handoff/`;
