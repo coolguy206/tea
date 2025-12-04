d2066807c89f554805333181e0872304c40b6dfa
David Thanphilom <davidthanphilom@msn.com>
2025-12-03 17:09:51 -0800

    promos/ coded banners 12.10.promo

---
+++ promos/default/js/black-friday.js
@@ -0,0 +1,12 @@
+//? CODE REFRENCE FOR BLACK FRIDAY PROMO MANAGEMENT
+
+//? BLACK FRIDAY ADD CLASS IF NEEDED
+$('.promos .the-promo').addClass('bf');
+
+//? BLACK FRIDAY HIDE PROMOS AND SHOW COUNTDON IF NEEDED
+$('.promos').css('display', 'none');
+$('.promos.countdown').css('display', 'block');
+
+//   //? CLONE COUNTDOWN AND ADD UNDER BANNER FOR BLACK FRIDAY
+  $('header.site-header .promos').after($('.the-promo.countdown-promo').clone());
+  $('header.site-header').prepend($('.promos.countdown').clone());
---
+++ promos/default/js/test-put-promo-footer-mobile.js
@@ -0,0 +1,16 @@
+//? CODE REFRENCE FOR PUTTING PROMO IN FOOTER ON MOBILE AND SHOW ON SCROLL
+
+//? on mobile clone the promo & put in the footer. when scroll show it
+$('.the-banner-footer-container').prepend($('.promos').clone());
+
+$(window).scroll(function () {
+
+    var scrollTop = $(this).scrollTop();
+    // console.log(scrollTop);
+
+    if (scrollTop > 216) {
+        $('.the-banner-footer-container .promos').addClass('show-the-banner');
+    } else {
+        $('.the-banner-footer-container .promos').removeClass('show-the-banner');
+    }
+});
