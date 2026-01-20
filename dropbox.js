57aa33299100bcf36b83a3155fcacd751fa37647
David Thanphilom <davidthanphilom@msn.com>
2026-01-20 11:50:25 -0800

    dropbox.js ftp.js homepage/ promos/ assets ready 01.21.promo

--- dropbox.js
+++ dropbox.js
@@ -15,13 +15,13 @@ refreshAccessToken((data) => {
     accessToken: access_token
   });
 
-  var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Promotions/01 january/011626 warehouse sale/04 handoff/site/version 2 (re-ordered tiers)/`;
+  var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Promotions/01 january/012126 14% Off/03 Handoff/Site/`;
   // var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Site/010626 Resort/03 Handoff/visual navs`;
   // var folderPath = `/Sam Khieu’s files/Shared/Design/2025/Campaign/121425 Tea x Tushbaby Giveaway/handoff/site/`
   folderPath = folderPath.toLowerCase();
   // console.log(folderPath);
 
-  var writeFilePath = `Site/0116/`;
+  var writeFilePath = `Site/0121/`;
   // var writeFilePath = `handoff/landing-pages/dresses/0210/`;
   writeFilePath = resolve(writeFilePath);
   // console.log(writeFilePath);
