9a95d0bab061b90067769b06bd2d3fc4f579aa72
David Thanphilom <davidthanphilom@msn.com>
2026-01-29 12:40:06 -0800

    dropbox.js ftp.js landing-page/email/ coded master

--- ftp.js
+++ ftp.js
@@ -6,17 +6,17 @@ const mkDir = util.promisify(fs.mkdir);
 require('dotenv').config()
 
 var year = `2026`;
-var num = `0128`;
+var num = `0202`;
 var version = `v0`;
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
 // var category = `promos/one-offs/${year}/${num}/${version}/hp/`;
-var category = `promos/one-offs/${year}/${num}/${version}/mega/`;
+// var category = `promos/one-offs/${year}/${num}/${version}/mega/`;
 // var category = `promos/giving-tuesday/${year}/${num}/${version}/`;
 // var category = `homepage/${year}/${num}/${version}/`;
 // var category = `headers/${year}/${num}/${version}/`;
-// var category = `landing-pages/tushbaby/${year}/${num}/${version}/`;
+var category = `landing-pages/email/${year}/${num}/${version}/`;
 // var category = `mega-menu/${year}/${num}/${version}/`;
 
 //!EXCEPT THIS ONE
@@ -30,9 +30,9 @@ var readFilePath = `Site/${num}/Site/mega/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
 // var writeFilePath = `promos/default/dev/images/homepage/${num}/`;
-var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
+// var writeFilePath = `promos/default/dev/images/mega-menu/${num}/`;
 // var writeFilePath = `promos/headers/dev/images/promo/print-shop/`;
-// var writeFilePath = `landing-page/sweeps/tushbaby-giveaway/dev/images/`;
+var writeFilePath = `landing-page/email/dev/images/`;
 // var writeFilePath = `homepage/default/dev/images/${num}/`;
 // var writeFilePath = `mega-menu/dev/images/`;
 var fileName = `c2b-m`;
@@ -128,7 +128,7 @@ c.on('ready', function () {
   */
 
   //?MAKE DIRECTORY THEN UPLOAD FILES AND MAKE HTML FILES
-  /*
+  
     c.mkdir(url, true, (err) => {
       if (err) throw err;
       console.log(`successfully made dir: ${url}`);
@@ -238,10 +238,10 @@ c.on('ready', function () {
       })
       // c.end();
     });
-  */
+  
 
   //?GET LIST OF FILES THEN COMPILE HTML FILES
-
+/*
   c.list(url, function (err, list) {
     if (err) throw err;
     list.map((file, i) => {
@@ -343,7 +343,7 @@ c.on('ready', function () {
 
     c.end();
   });
-
+*/
 
   //?GET LIST OF FILES THEN DELETE FILES IN DIRECTORY
   /*
