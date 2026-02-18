e01961ef6eec67c2c3333ad32022c8ed40d8ddc1
David Thanphilom <davidthanphilom@msn.com>
2026-02-17 17:31:09 -0800

    dropbox.js ftp.js assets ready 02.20.promo

--- ftp.js
+++ ftp.js
@@ -6,8 +6,8 @@ const mkDir = util.promisify(fs.mkdir);
 require('dotenv').config()
 
 var year = `2026`;
-var num = `0213`;
-var version = `v1`;
+var num = `0220`;
+var version = `v0`;
 
 //!NEEDS ENDING SLASH
 var baseURL = `/media/tea_collection/`;
@@ -24,8 +24,9 @@ var url = `${baseURL}${category}`
 
 //!NEEDS ENDING SLASH
 // var readFilePath = `Site/${num}/Site/headers/Print Shop/`;
-var readFilePath = `Site/${num}/Site/HP-g`;
-// var readFilePath = `Site/${num}/Site/mega/`;
+var readFilePath = `Site/${num}/Site/hp`;
+// var readFilePath = `Site/${num}/Site/promo/hp/`;
+// var readFilePath = `Site/${num}/Site/promo/mega/`;
 // var readFilePath = `handoff/landing-pages/dresses/${num}/handoff/`;
 
 // var writeFilePath = `promos/bubble/dev/images/`;
@@ -128,222 +129,222 @@ c.on('ready', function () {
   */
 
   //?MAKE DIRECTORY THEN UPLOAD FILES AND MAKE HTML FILES
-  /*
-    c.mkdir(url, true, (err) => {
+
+  c.mkdir(url, true, (err) => {
+    if (err) throw err;
+    console.log(`successfully made dir: ${url}`);
+
+    //*read the files on local machine
+    fs.readdir(`${readFilePath}`, (err, files) => {
       if (err) throw err;
-      console.log(`successfully made dir: ${url}`);
-  
-      //*read the files on local machine
-      fs.readdir(`${readFilePath}`, (err, files) => {
-        if (err) throw err;
-        // console.log("\nCurrent directory filenames:");
-        // console.log(files);
-        files.map((file, i) => {
-          // console.log(`${readFilePath}${file}`);
-          // console.log(`${url}${file}`)
-          // console.log(file);
-  
-          //*upload the file
-          c.put(`${readFilePath}${file}`, `${url}${file}`, (err) => {
-            if (err) throw err;
-            console.log(`successfully uploaded file: ${file}`);
-  
-            var html = ``;
-            var name = file.split('.')[0];
-            var ext = file.split('.')[1];
-            var fileName = name.toLowerCase();
-            // console.log(ext);
-  
-            if (ext == 'pdf') {
-              // console.log(`it is a pdf`);
-              var html = `<a class="the-official-rules" href="/mas_assets${url}${file}" target="_blank">official rules</a>`;
-            } else if (ext == 'mp4') {
-              console.log(`it is a mp4`);
-              if (name.indexOf(`-d`) !== -1) {
+      // console.log("\nCurrent directory filenames:");
+      // console.log(files);
+      files.map((file, i) => {
+        // console.log(`${readFilePath}${file}`);
+        // console.log(`${url}${file}`)
+        // console.log(file);
+
+        //*upload the file
+        c.put(`${readFilePath}${file}`, `${url}${file}`, (err) => {
+          if (err) throw err;
+          console.log(`successfully uploaded file: ${file}`);
+
+          var html = ``;
+          var name = file.split('.')[0];
+          var ext = file.split('.')[1];
+          var fileName = name.toLowerCase();
+          // console.log(ext);
+
+          if (ext == 'pdf') {
+            // console.log(`it is a pdf`);
+            var html = `<a class="the-official-rules" href="/mas_assets${url}${file}" target="_blank">official rules</a>`;
+          } else if (ext == 'mp4') {
+            console.log(`it is a mp4`);
+            if (name.indexOf(`-d`) !== -1) {
               var html = `
                 <video poster="" class="desktop" muted autoplay loop>
                   <source src="/mas_assets${url}${file}" type="video/mp4">
                   Your browser does not support the video tag.
                 </video>
                 `;
-              } else if (name.indexOf(`-m`) !== -1) {
-                var html = `
+            } else if (name.indexOf(`-m`) !== -1) {
+              var html = `
                 <video poster="" class="mobile" muted autoplay loop playsinline> 
                   <source src="/mas_assets${url}${file}" type="video/mp4">
                   Your browser does not support the video tag.
                 </video>
                 `;
-              } else {
-                var html = `
-                <video poster="" muted autoplay loop>
-                  <source src="/mas_assets${url}${file}" type="video/mp4">
-                  Your browser does not support the video tag.
-                </video>
-                `;
-              }
-              // console.log(`${url} ${file}`);
             } else {
-              // console.log(`it is not a pdf`);
-              var alt = ``;
-              if (name.indexOf(`-`) !== -1) {
-                // console.log(name);
-                alt = name.split(`-`)[0];
-                alt = alt.replace(/_/g, ' ');
-                // console.log(alt);
-                if (name.indexOf(`-d`) !== -1) {
-                  html = `<img class="desktop" data-set="/mas_assets${url}${file}" alt="${alt}">`;
-                } else if (name.indexOf(`-m`) !== -1) {
-                  html = `<img class="mobile" data-set="/mas_assets${url}${file}" alt="${alt}">`;
-                } else {
-                  html = `<img data-set="/mas_assets${url}${file}">`;
-                }
-              } else {
-                alt = name.replace(/_/g, ' ');
-                // console.log(alt);
-                html = `<img data-set="/mas_assets${url}${file}" alt="${alt}">`;
-              }
-  
-            }
-  
-            // console.log(`the html`);
-            // console.log(html);
-            // console.log(`${writeFilePath}${fileName}.html`);
-  
-            //*make the html file
-            writeFileContent(`${writeFilePath}${fileName}.html`, html).then((data) => {
-              console.log(`File written successfully. ${fileName}.html`);
-            }).catch((err) => {
-              console.log(err);
-              if (err.code == `ENOENT`) {
-                console.log(`let's mkdir`);
-                mkDir(writeFilePath, true).then((data) => {
-                  console.log('Directory created successfully!');
-                  console.log(writeFilePath);
-  
-                  writeFileContent(`${writeFilePath}${fileName}.html`, html).then((data) => {
-                    console.log(`File written successfully. ${fileName}.html`);
-                  }).catch((err) => {
-                    console.log(err);
-                  })
-                })
-                  .catch((err) => {
-                    console.log(err);
-                  })
-              }
-            });
-            c.end();
-          });
-  
-        });
-      })
-      // c.end();
-    });
-  */
-
-  //?GET LIST OF FILES THEN COMPILE HTML FILES
-
-  c.list(url, function (err, list) {
-    if (err) throw err;
-    list.map((file, i) => {
-
-      // console.log(val);
-
-      var html = ``;
-      var name = file.name.split('.')[0];
-      var ext = file.name.split('.')[1];
-      var fileName = name.toLowerCase();
-      // console.log(ext);
-
-      if (ext == 'pdf') {
-
-        // console.log(`it is a pdf`);
-        var html = `<a class="the-official-rules" href="/mas_assets${url}${file.name}" target="_blank">official rules</a>`;
-
-      } else if (ext == 'mp4') {
-
-        console.log(`it is a mp4`);
-
-        if (name.indexOf(`-d`) !== -1) {
-          var html = `
-                <video poster="" class="desktop" muted autoplay loop>
-                  <source src="/mas_assets${url}${file.name}" type="video/mp4">
-                  Your browser does not support the video tag.
-                </video>
-                `;
-        } else if (name.indexOf(`-m`) !== -1) {
-          var html = `
-                <video poster="" class="mobile" muted autoplay loop playsinline> 
-                  <source src="/mas_assets${url}${file.name}" type="video/mp4">
-                  Your browser does not support the video tag.
-                </video>
-                `;
-        } else {
-          var html = `
+              var html = `
                 <video poster="" muted autoplay loop>
-                  <source src="/mas_assets${url}${file.name}" type="video/mp4">
+                  <source src="/mas_assets${url}${file}" type="video/mp4">
                   Your browser does not support the video tag.
                 </video>
                 `;
-        }
-        // console.log(`${url} ${file}`);
-      } else {
-        // console.log(`it is not a pdf`);
-        var alt = ``;
-        if (name.indexOf(`-`) !== -1) {
-          // console.log(name);
-          alt = name.split(`-`)[0];
-          alt = alt.replace(/_/g, ' ');
-          // console.log(alt);
-          if (name.indexOf(`-d`) !== -1) {
-            html = `<img class="desktop" data-set="/mas_assets${url}${file.name}" alt="${alt}">`;
-          } else if (name.indexOf(`-m`) !== -1) {
-            html = `<img class="mobile" data-set="/mas_assets${url}${file.name}" alt="${alt}">`;
+            }
+            // console.log(`${url} ${file}`);
           } else {
-            html = `<img data-set="/mas_assets${url}${file.name}">`;
+            // console.log(`it is not a pdf`);
+            var alt = ``;
+            if (name.indexOf(`-`) !== -1) {
+              // console.log(name);
+              alt = name.split(`-`)[0];
+              alt = alt.replace(/_/g, ' ');
+              // console.log(alt);
+              if (name.indexOf(`-d`) !== -1) {
+                html = `<img class="desktop" data-set="/mas_assets${url}${file}" alt="${alt}">`;
+              } else if (name.indexOf(`-m`) !== -1) {
+                html = `<img class="mobile" data-set="/mas_assets${url}${file}" alt="${alt}">`;
+              } else {
+                html = `<img data-set="/mas_assets${url}${file}">`;
+              }
+            } else {
+              alt = name.replace(/_/g, ' ');
+              // console.log(alt);
+              html = `<img data-set="/mas_assets${url}${file}" alt="${alt}">`;
+            }
+
           }
-        } else {
-          alt = name.replace(/_/g, ' ');
-          // console.log(alt);
-          html = `<img data-set="/mas_assets${url}${file.name}" alt="${alt}">`;
-        }
 
-      }
+          // console.log(`the html`);
+          // console.log(html);
+          // console.log(`${writeFilePath}${fileName}.html`);
 
-      // console.log(`the html`);
-      // console.log(html);
-      // console.log(`${writeFilePath}${fileName}.html`);
+          //*make the html file
+          writeFileContent(`${writeFilePath}${fileName}.html`, html).then((data) => {
+            console.log(`File written successfully. ${fileName}.html`);
+          }).catch((err) => {
+            console.log(err);
+            if (err.code == `ENOENT`) {
+              console.log(`let's mkdir`);
+              mkDir(writeFilePath, true).then((data) => {
+                console.log('Directory created successfully!');
+                console.log(writeFilePath);
 
-      //*make the html file
-      writeFileContent(`${writeFilePath}${name}.html`, html).then((data) => {
-        console.log(`File written successfully. ${name}.html`);
-      }).catch((err) => {
-        console.log(err);
-
-        if (err.code == `ENOENT`) {
-          console.log(`let's mkdir`);
-          mkDir(writeFilePath, true).then((data) => {
-            console.log('Directory created successfully!');
-            console.log(writeFilePath);
-
-            writeFileContent(`${writeFilePath}${fileName}.html`, html).then((data) => {
-              console.log(`File written successfully. ${fileName}.html`);
-            }).catch((err) => {
-              console.log(err);
-            })
-          })
-            .catch((err) => {
-              console.log(err);
-            })
-        }
+                writeFileContent(`${writeFilePath}${fileName}.html`, html).then((data) => {
+                  console.log(`File written successfully. ${fileName}.html`);
+                }).catch((err) => {
+                  console.log(err);
+                })
+              })
+                .catch((err) => {
+                  console.log(err);
+                })
+            }
+          });
+          c.end();
+        });
 
       });
-
-
     })
-
-    c.end();
+    // c.end();
   });
 
+
+  //?GET LIST OF FILES THEN COMPILE HTML FILES
+  /*
+    c.list(url, function (err, list) {
+      if (err) throw err;
+      list.map((file, i) => {
+  
+        // console.log(val);
+  
+        var html = ``;
+        var name = file.name.split('.')[0];
+        var ext = file.name.split('.')[1];
+        var fileName = name.toLowerCase();
+        // console.log(ext);
+  
+        if (ext == 'pdf') {
+  
+          // console.log(`it is a pdf`);
+          var html = `<a class="the-official-rules" href="/mas_assets${url}${file.name}" target="_blank">official rules</a>`;
+  
+        } else if (ext == 'mp4') {
+  
+          console.log(`it is a mp4`);
+  
+          if (name.indexOf(`-d`) !== -1) {
+            var html = `
+                  <video poster="" class="desktop" muted autoplay loop>
+                    <source src="/mas_assets${url}${file.name}" type="video/mp4">
+                    Your browser does not support the video tag.
+                  </video>
+                  `;
+          } else if (name.indexOf(`-m`) !== -1) {
+            var html = `
+                  <video poster="" class="mobile" muted autoplay loop playsinline> 
+                    <source src="/mas_assets${url}${file.name}" type="video/mp4">
+                    Your browser does not support the video tag.
+                  </video>
+                  `;
+          } else {
+            var html = `
+                  <video poster="" muted autoplay loop>
+                    <source src="/mas_assets${url}${file.name}" type="video/mp4">
+                    Your browser does not support the video tag.
+                  </video>
+                  `;
+          }
+          // console.log(`${url} ${file}`);
+        } else {
+          // console.log(`it is not a pdf`);
+          var alt = ``;
+          if (name.indexOf(`-`) !== -1) {
+            // console.log(name);
+            alt = name.split(`-`)[0];
+            alt = alt.replace(/_/g, ' ');
+            // console.log(alt);
+            if (name.indexOf(`-d`) !== -1) {
+              html = `<img class="desktop" data-set="/mas_assets${url}${file.name}" alt="${alt}">`;
+            } else if (name.indexOf(`-m`) !== -1) {
+              html = `<img class="mobile" data-set="/mas_assets${url}${file.name}" alt="${alt}">`;
+            } else {
+              html = `<img data-set="/mas_assets${url}${file.name}">`;
+            }
+          } else {
+            alt = name.replace(/_/g, ' ');
+            // console.log(alt);
+            html = `<img data-set="/mas_assets${url}${file.name}" alt="${alt}">`;
+          }
+  
+        }
+  
+        // console.log(`the html`);
+        // console.log(html);
+        // console.log(`${writeFilePath}${fileName}.html`);
+  
+        //*make the html file
+        writeFileContent(`${writeFilePath}${name}.html`, html).then((data) => {
+          console.log(`File written successfully. ${name}.html`);
+        }).catch((err) => {
+          console.log(err);
+  
+          if (err.code == `ENOENT`) {
+            console.log(`let's mkdir`);
+            mkDir(writeFilePath, true).then((data) => {
+              console.log('Directory created successfully!');
+              console.log(writeFilePath);
+  
+              writeFileContent(`${writeFilePath}${fileName}.html`, html).then((data) => {
+                console.log(`File written successfully. ${fileName}.html`);
+              }).catch((err) => {
+                console.log(err);
+              })
+            })
+              .catch((err) => {
+                console.log(err);
+              })
+          }
+  
+        });
+  
+  
+      })
+  
+      c.end();
+    });
+  */
 
   //?GET LIST OF FILES THEN DELETE FILES IN DIRECTORY
   /*
