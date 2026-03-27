59e1d570775ab36ebeaf30ff7dedd4159e3ce98b
David Thanphilom <davidthanphilom@msn.com>
2026-03-27 12:56:01 -0700

    homepage/ promos/ coded 03.31.promo

--- promos/default/js/browserify/scripts.js
+++ promos/default/js/browserify/scripts.js
@@ -68,9 +68,9 @@ jQuery(document).ready(function () {
       // $(`.promos .the-promo`).css('height', '46px');
       // $(`.promos .the-promo span.third, .promos .the-promo span > a.third`).hide()
       // $('.promos .the-promo.promo1').hide();
-      $('.promos .the-promo.promo1').remove();
+      // $('.promos .the-promo.promo1').remove();
     } else {
-      $('.promos .the-promo.shipping-promo').remove();
+      // $('.promos .the-promo.shipping-promo').remove();
     }
   } else {
     //? NOT HOME PAGE
@@ -141,7 +141,8 @@ jQuery(document).ready(function () {
     }
     // console.log(`fancy closed clicked`);
   });
-  (0, _makeBxslider.makeBxSlider)('.promos-wrap');
+
+  // makeBxSlider('.promos-wrap');
 });
 
 },{"./make-bxslider.js":2}]},{},[3]);
