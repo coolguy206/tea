d3faaf3af169e81f495cee966bca970908e4d415
David Thanphilom <davidthanphilom@msn.com>
2026-04-22 16:21:49 -0700

    dropbox.js ftp.js landing-page/ still coding 04.23.summer2

--- dropbox.js
+++ dropbox.js
@@ -16,8 +16,8 @@ refreshAccessToken((data) => {
   });
 
   // var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Promotions/04 April/041026 Friends & Family/Handoff/Site/extra 10/`;
-  var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Site/042326 Summer 2/handoff/`;
-  // var folderPath = `/Sam Khieu’s files/Shared/Design/2025/Campaign/121425 Tea x Tushbaby Giveaway/handoff/site/`
+  // var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Site/042326 Summer 2/handoff/`;
+  var folderPath = `/Sam Khieu’s files/Shared/Design/2026/Campaign/American Girl/handoff/042226 Sign Up Page/`
   folderPath = folderPath.toLowerCase();
   // console.log(folderPath);
 
