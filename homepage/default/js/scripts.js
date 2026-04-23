74dac47d0c23a4f88f53e5c1975edc0b0a9054cd
David Thanphilom <davidthanphilom@msn.com>
2026-04-23 16:44:32 -0700

    content-zone/sliders/ homepage/ promos/ coded 04.26.promo

--- homepage/default/js/scripts.js
+++ homepage/default/js/scripts.js
@@ -17,6 +17,8 @@ $(document).ajaxComplete(function (event, xhr, options) {
 
   cloneExecuteSlider('.new-arrivals-slider.all', '.shop-slideshow-3');
 
+   cloneExecuteSlider('.the-swim-slider', '.shop-slideshow-4');
+
   $('.hp .white-out').fadeOut();
   // $('.the-sliders').hide();
 
@@ -58,7 +60,7 @@ $(document).ready(function () {
   // });
 
 
-  inview('.hp .promo1-skinny, .hp .promo2-skinny, .hp .promo1, .hp .promo2, .hp .promo3, .hp .promo4, .hp .promos-2-cell, .hp .c1, .hp .c2, .hp .c2-slideshow, .hp .c3, .hp .c4, .hp .c5, .hp .c6, .hp .c7, .hp .c8, .hp .graphics, .hp .refer-friend, .hp .rewear, .hp .shop-slideshow, .hp .shop-slideshow2, .hp .shop-slideshow3, .hp .why-tea');
+  inview('.hp .promo1-skinny, .hp .promo2-skinny, .hp .promo1, .hp .promo2, .hp .promo3, .hp .promo4, .hp .promos-2-cell, .hp .c1, .hp .c2, .hp .c2-slideshow, .hp .c3, .hp .c4, .hp .c5, .hp .c6, .hp .c7, .hp .c8, .hp .graphics, .hp .refer-friend, .hp .rewear, .hp .shop-slideshow, .hp .shop-slideshow2, .hp .shop-slideshow3, .hp .shop-slideshow4, .hp .why-tea');
 
 
   for (var i = 1; i < 9; i++) {
@@ -68,6 +70,8 @@ $(document).ready(function () {
 
   tracking('.hp .shop-slideshow a', 'shop-slideshow');
   tracking('.hp .shop-slideshow2 a', 'shop-slideshow2');
+  tracking('.hp .shop-slideshow3 a', 'shop-slideshow3');
+  tracking('.hp .shop-slideshow4 a', 'shop-slideshow4');
   tracking('.hp .promo1 a', 'promo1');
 
 
